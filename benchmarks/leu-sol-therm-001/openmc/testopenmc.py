adea5606850a84c4d5ee4c909e3cbf12bb254b74
Rohan-TG <rohan.tg05@gmail.com>
2025-06-12 16:12:02 +0100

    testing on fermiac

---
+++ benchmarks/leu-sol-therm-001/openmc/testopenmc.py
@@ -0,0 +1,10 @@
+import os
+import h5py
+import openmc.data
+import urllib.request
+
+url = 'https://anl.box.com/shared/static/kxm7s57z3xgfbeq29h54n7q6js8rd11c.ace'
+filename, headers = urllib.request.urlretrieve(url, 'gd157.ace')
+
+# Load ACE data into object
+gd157 = openmc.data.IncidentNeutron.from_ace('gd157.ace')
