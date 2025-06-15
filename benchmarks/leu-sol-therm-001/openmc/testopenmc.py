952bd4dd35282ec422dbfc294a7ed26eb0a31962
Rohan-TG <rohan.tg05@gmail.com>
2025-06-15 18:58:52 +0100

    need to generate ACE file

--- benchmarks/leu-sol-therm-001/openmc/testopenmc.py
+++ benchmarks/leu-sol-therm-001/openmc/testopenmc.py
@@ -1,10 +1,12 @@
-# import os
-# import h5py
-# import openmc.data
-# import urllib.request
-#
+import os
+import h5py
+import openmc.data
+import urllib.request
+
 # url = 'https://anl.box.com/shared/static/kxm7s57z3xgfbeq29h54n7q6js8rd11c.ace'
 # filename, headers = urllib.request.urlretrieve(url, 'gd157.ace')
-#
+h1 = openmc.data.IncidentNeutorn.from_endf('n-001_H_001_test1.endf')
 # # Load ACE data into object
 # gd157 = openmc.data.IncidentNeutron.from_ace('gd157.ace')
+
+
