b3c5d4837b1be4b85ba33d9fa84a4b5aaa963641
Rohan-TG <rohan.tg05@gmail.com>
2025-06-15 19:00:21 +0100

    typo

--- benchmarks/leu-sol-therm-001/openmc/testopenmc.py
+++ benchmarks/leu-sol-therm-001/openmc/testopenmc.py
@@ -5,7 +5,7 @@ import urllib.request
 
 # url = 'https://anl.box.com/shared/static/kxm7s57z3xgfbeq29h54n7q6js8rd11c.ace'
 # filename, headers = urllib.request.urlretrieve(url, 'gd157.ace')
-h1 = openmc.data.IncidentNeutorn.from_endf('n-001_H_001_test1.endf')
+h1 = openmc.data.IncidentNeutron.from_endf('n-001_H_001_test1.endf')
 # # Load ACE data into object
 # gd157 = openmc.data.IncidentNeutron.from_ace('gd157.ace')
 
