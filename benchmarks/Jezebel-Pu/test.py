b4a1a00b718b00b3ec4e2589f77642d373ef5b16
Rohan-TG <rohan.tg05@gmail.com>
2025-07-16 21:49:14 +0100

    tabbing

--- benchmarks/Jezebel-Pu/test.py
+++ benchmarks/Jezebel-Pu/test.py
@@ -4,10 +4,12 @@ import h5py
 
 # Open the file in read-only mode
 with h5py.File('statepoint.3050.h5', 'r') as f:
-    # List all groups
-    print("Keys: %s" % f.keys())
-    group = list(f.keys())[0]
+	# List all groups
+	print("Keys: %s" % f.keys())
+	group = list(f.keys())[0]
 
-    # Access a dataset
-    data = f['k_combined']
-    print(data)
+	# Access a dataset
+	data = f['k_combined']
+	print(data)
+	allk = data[:]
+	print(allk)
