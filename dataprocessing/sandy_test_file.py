fec2db19fa17b1ebbe684090a8bdaec4e972d899
Rohan-TG <rohan.tg05@gmail.com>
2025-07-20 22:59:13 +0100

    first test

--- dataprocessing/sandy_test_file.py
+++ dataprocessing/sandy_test_file.py
@@ -1,1 +1,9 @@
 import sandy
+import pandas as pd
+from os.path import join
+import numpy as np
+import random, sys
+import matplotlib.pyplot as plt
+
+za = 94239
+tape = sandy.get_endf6_file("n-094_Pu_239.endf", "nfpy", za * 10)
