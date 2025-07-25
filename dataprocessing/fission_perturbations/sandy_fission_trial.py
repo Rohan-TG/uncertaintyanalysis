d2e022dc3bb86a6481e94165a4ed005510032453
Rohan-TG <rohan.tg05@gmail.com>
2025-07-25 21:31:41 +0100

    attempting to perturb fission only - first attempt is 0 perturbations test case

--- dataprocessing/fission_perturbations/sandy_fission_trial.py
+++ dataprocessing/fission_perturbations/sandy_fission_trial.py
@@ -15,59 +15,79 @@ import time
 start = time.time()
 
 za = 94239
-filename = "n-094_Pu_239.endf"
+# filename = "n-094_Pu_239.endf"
 
-tape = sandy.get_endf6_file("ENDFB_80", "xs", za * 10).get_pendf(temperature = 300, purr=True, heatr=False, gaspr=False, verbose=True)
-xs = sandy.Xs.from_endf6(tape)
+endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10).get_pendf(temperature = 300, purr=True, heatr=False, gaspr=False, verbose=True)
+pendf = endf6.get_pendf(err=0.00001, verbose=True)
+
+xs = sandy.Xs.from_endf6(pendf)
+
+lower_bound = 1e-5  # eV
+upper_bound = 2e7   # eV
+domain = [lower_bound, upper_bound]
 
 
 mat = 9437
 mt = 18
 perturbation_coefficient = 0
 
-perturbed_xs = []
-for i in range(1, egrid.size):
-    e_start = egrid[i-1]
-    e_stop = egrid[i]
-    index = egrid[i-1: i+1]
-    pert = sandy.Pert([1, 1 + pert_coeff], index=index)
-    print(f"perturbed xs in energy bin #{i} [{e_start:.5e}, {e_stop:.5e}]")
-    xspert = xs.custom_perturbation(mat, mt, pert)
-    perturbed_xs.append(xspert)
-
-tape.to_file(filename)
-
-number_of_samples = 1
-
-processes = 1
-
-cli = f"{filename}  --processes {processes}  --samples {number_of_samples}  --mf 33  --temperatures 300  --acer  --debug"
-sandy.sampling.run(cli.split())
 
 
+perturbation = sandy.Pert([1, 1 + perturbation_coefficient], index=domain)
 
-smps = tape.get_perturbations(
-    number_of_samples,
-    njoy_kws=dict(
-        err=10,   # very fast calculation, for testing
-        chi=False,
-        mubar=False,
-        xs=True,
-        nubar=False,
-        verbose=True,
-    ),
-)
+xspert = xs.custom_perturbation(mat, mt, perturbation)
+
+pendf_pert = xspert.to_endf6(pendf) # Create PENDF of perturbed data
+
+tag = "_pert"
+outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
+with open("94239_unperturbed_test.09c", mode="w") as f:
+    f.write(outs["ace"])
 
 
-outs = tape.apply_perturbations(
-    smps,
-    processes=processes,
-    njoy_kws=dict(err=1),   # very fast calculation, for testing
-    to_ace=True,   # produce ACE files
-    to_file=True,
-    ace_kws=dict(err=1, temperature=300, verbose=True, purr=False, heatr=False, thermr=False, gaspr=False),
-    verbose=True,
-)
+# perturbed_xs = []
+# for i in range(1, egrid.size):
+#     e_start = egrid[i-1]
+#     e_stop = egrid[i]
+#     index = egrid[i-1: i+1]
+#     pert = sandy.Pert([1, 1 + perturbation_coefficient], index=index)
+#     print(f"perturbed xs in energy bin #{i} [{e_start:.5e}, {e_stop:.5e}]")
+#     xspert = xs.custom_perturbation(mat, mt, pert)
+#     perturbed_xs.append(xspert)
+
+# tape.to_file(filename)
+
+# number_of_samples = 1
+#
+# processes = 1
+#
+# cli = f"{filename}  --processes {processes}  --samples {number_of_samples}  --mf 33  --temperatures 300  --acer  --debug"
+# sandy.sampling.run(cli.split())
+
+
+
+# smps = tape.get_perturbations(
+#     number_of_samples,
+#     njoy_kws=dict(
+#         err=10,   # very fast calculation, for testing
+#         chi=False,
+#         mubar=False,
+#         xs=True,
+#         nubar=False,
+#         verbose=True,
+#     ),
+# )
+#
+#
+# outs = tape.apply_perturbations(
+#     smps,
+#     processes=processes,
+#     njoy_kws=dict(err=1),   # very fast calculation, for testing
+#     to_ace=True,   # produce ACE files
+#     to_file=True,
+#     ace_kws=dict(err=1, temperature=300, verbose=True, purr=False, heatr=False, thermr=False, gaspr=False),
+#     verbose=True,
+# )
 
 end = time.time()
 
