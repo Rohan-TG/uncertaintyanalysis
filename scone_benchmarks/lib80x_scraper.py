03cd6c46d7f7343b9a64f4b2c5d32a2ef019fd5f
Rohan-TG <rohan.tg05@gmail.com>
2025-07-26 16:29:40 +0100

    testing copy

---
+++ scone_benchmarks/lib80x_scraper.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import tqdm
+
+Lib80x_dir = "/home/rnt26/scone/SCONE/Lib80x/Lib80x/Lib80x"
+
+elements = os.listdir(Lib80x_dir)
+
+endfb8_ace_dir = "/home/rnt26/PycharmProjects/uncertaintyanalysis/data/ENDFBVIII/endfb-viii.0-ace"
+
+suffix = '.800nc'
+
+for Element in tqdm.tqdm(elements, total=len(elements)):
+	element_dir_path = os.path.join(Lib80x_dir, Element)
+	if os.path.isdir(element_dir_path):
+		all_elemental_ace = os.listdir(element_dir_path)
+		for isotope_ace in all_elemental_ace:
+			if suffix in isotope_ace:
+				shutil.copy(isotope_ace, endfb8_ace_dir)
+
