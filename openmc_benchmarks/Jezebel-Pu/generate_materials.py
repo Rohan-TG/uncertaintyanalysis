f3893b11d6c3e05812dbc7cd2b8f7bf0b41fb2ec
Rohan-TG <rohan.tg05@gmail.com>
2025-07-26 17:22:04 +0100

    added input for openmc

--- openmc_benchmarks/Jezebel-Pu/generate_materials.py
+++ openmc_benchmarks/Jezebel-Pu/generate_materials.py
@@ -1,5 +1,5 @@
 import os
-n_cores = 60
+n_cores = int(input("Enter core number: "))
 os.environ["OMP_NUM_THREADS"] = f"{n_cores}"
 os.environ["MKL_NUM_THREADS"] = f"{n_cores}"
 os.environ["OPENBLAS_NUM_THREADS"] = f"{n_cores}"
