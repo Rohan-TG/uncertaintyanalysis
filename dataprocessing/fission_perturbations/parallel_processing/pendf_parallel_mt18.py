fa411c976cc72115d6d375ecedf7eae0be885acf
Rohan-TG <rohan.tg05@gmail.com>
2025-07-29 15:08:38 +0100

    0.3 to 0.6

--- dataprocessing/fission_perturbations/parallel_processing/pendf_parallel_mt18.py
+++ dataprocessing/fission_perturbations/parallel_processing/pendf_parallel_mt18.py
@@ -16,8 +16,8 @@ za = 94239
 # perturbation_coefficients = np.arange(-0.599, -0.4, 0.001)
 # perturbation_coefficients = np.arange(-0.4, -0.2, 0.001)
 # perturbation_coefficients = np.arange(-0.2, 0.0, 0.001)
-perturbation_coefficients = np.arange(0.0, 0.3, 0.001)
-# perturbation_coefficients = np.arange(0.3, 0.6, 0.001)
+# perturbation_coefficients = np.arange(0.0, 0.3, 0.001)
+perturbation_coefficients = np.arange(0.3, 0.6, 0.001)
 
 # perturbation_coefficients = np.arange(0.6, 1.001, 0.001)
 
