88a6d029ab08fd2cc9b091cdff84a4f7181de24c
Rohan-TG <rohan.tg05@gmail.com>
2025-07-30 16:53:38 +0100

    readfile working

--- scone_benchmarks/flat_mt18_perturbations_Jezebel/process_1/run_scone_1.py
+++ scone_benchmarks/flat_mt18_perturbations_Jezebel/process_1/run_scone_1.py
@@ -1,1 +1,15 @@
 import os
+# import numpy as np
+
+
+
+libfile = 'lib1.xsfile'
+with open(libfile, 'r') as file:
+	lines = file.readlines()
+
+Pu_239_address = 511
+lines[Pu_239_address] = 'new pu address test\n'
+
+
+with open(libfile, 'w') as file:
+	file.writelines(lines)
