da6d901844fafd21d1d33c52d3d48c719f06d9ca
Rohan-TG <rohan.tg05@gmail.com>
2025-08-01 14:05:32 +0100

    testing keff

--- scone_benchmarks/flatp_mt18_Jezebel/p_1/run_scone_1.py
+++ scone_benchmarks/flatp_mt18_Jezebel/p_1/run_scone_1.py
@@ -10,10 +10,10 @@ ACE_file_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/data/Pu-23
 
 scone_executable_path = '/home/rnt26/scone/SCONE/Build/scone.out' # location of the scone executable
 
-num_cores = 15 # number of cores to use for this specific instance of scone
+num_cores = 20 # number of cores to use for this specific instance of scone
 
 # perturbation_coefficients = np.arange(-0.8, 1.001, 0.001)
-perturbation_coefficients = np.arange(-0.800, 0.100, 0.001)
+perturbation_coefficients = np.arange(-0.200, 0.100, 0.001)
 # perturbation_coefficients = np.arange(0.100, 1.001, 0.001)
 
 
