8478eb6745beb971efbb24de4ab682999069e8c2
Rohan-TG <rohan.tg05@gmail.com>
2025-08-04 15:19:01 +0100

    group 19

---
+++ dataprocessing/fission_perturbations/ECCO33_perturbations/group_19/group19_SANDY.py
@@ -0,0 +1,65 @@
+import sandy
+import subprocess
+import numpy as np
+# from concurrent.futures import ThreadPoolExecutor, as_completed
+# import random
+# from os.path import join
+import datetime
+import time
+import tqdm
+
+start = time.time()
+
+za = 94239
+
+
+perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
+
+
+endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
+pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
+pendf = endf6.get_pendf(err=0.0001, verbose=True)
+
+xs = sandy.Xs.from_endf6(pendf)
+
+lower_bound = 1.2340980000e3  # group 19 eV
+upper_bound = 2.0346840000e3   # group 18 eV
+
+domain = [lower_bound, upper_bound]
+
+
+mat = 9437
+mt = 18
+
+
+for coeff in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coefficients)):
+
+    perturbation = sandy.Pert([1, 1 + coeff], index=domain)
+
+    xspert = xs.custom_perturbation(mat, mt, perturbation)
+
+    pendf_pert = xspert.to_endf6(pendf) # Create PENDF of perturbed data
+    heated_pendf_pert = xspert.to_endf6(pendfheated)
+
+    tag = "_pert"
+    outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
+
+    savefilename = f"ECCO33-g19_Pu9_{coeff:0.3f}_MT18.09c"
+    with open(f"{savefilename}", mode="w") as f:
+        f.write(outs["ace"])
+
+    savefilependf = f"ECCO33-g19_Pu9_{coeff:0.3f}_MT18.pendf"
+    heated_pendf_pert.to_file(savefilependf)
+
+
+
+
+
+end = time.time()
+
+elapsed = end - start
+print(f"Time elapsed: {datetime.timedelta(seconds=elapsed)}")
+
+
+
+
