6e66f03a0399f95de0a06927b730eccc4512f247
Rohan-TG <rohan.tg05@gmail.com>
2025-08-04 12:24:39 +0100

    debugging njoy issue

--- dataprocessing/fission_perturbations/ECCO33_perturbations/group_pert_SANDY.py
+++ dataprocessing/fission_perturbations/ECCO33_perturbations/group_pert_SANDY.py
@@ -18,7 +18,8 @@ za = 94239
 perturbation_coefficients = [-0.3, -0.1, 0.1, 0.2, 0.3, 0.4]
 
 endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
-pendf = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
+pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
+pendf = endf6.get_pendf(err=0.0001, verbose=True)
 
 xs = sandy.Xs.from_endf6(pendf)
 
@@ -38,16 +39,17 @@ for coeff in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coeffic
     xspert = xs.custom_perturbation(mat, mt, perturbation)
 
     pendf_pert = xspert.to_endf6(pendf) # Create PENDF of perturbed data
+    heated_pendf_pert = xspert.to_endf6(pendfheated)
 
     tag = "_pert"
     outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
 
-    savefilename = f"ECCO13-14_Pu-239_coeff_{coeff:0.3f}_MT18.09c"
+    savefilename = f"ECCO33-14_Pu-239_coeff_{coeff:0.3f}_MT18.09c"
     with open(f"{savefilename}", mode="w") as f:
         f.write(outs["ace"])
 
-    savefilependf = f"ECCO13-14_Pu-239_coeff_{coeff:0.3f}_MT18.pendf"
-    pendf_pert.to_file()
+    savefilependf = f"ECCO33-14_Pu-239_coeff_{coeff:0.3f}_MT18.pendf"
+    heated_pendf_pert.to_file()
 
 
 
