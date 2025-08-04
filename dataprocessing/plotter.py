41198df3b3363d9d693ae30e8b863f7e03c45594
Rohan-TG <rohan.tg05@gmail.com>
2025-08-04 13:09:03 +0100

    plotting locally

--- dataprocessing/plotter.py
+++ dataprocessing/plotter.py
@@ -6,31 +6,37 @@ import ENDF6
 
 pert0 = 'plotdata_Pu-239_coeff_0.000_MT18.pendf'
 
-pert_m05 = 'plotdata_Pu-239_coeff_-0.500_MT18.pendf'
-
-pert03 = 'plotdata_Pu-239_coeff_0.300_MT18.pendf'
-
+# pert_m05 = 'plotdata_Pu-239_coeff_-0.500_MT18.pendf'
+#
+# pert03 = 'plotdata_Pu-239_coeff_0.300_MT18.pendf'
+pertgroup = '/home/rnt26/PycharmProjects/uncertaintyanalysis/dataprocessing/fission_perturbations/ECCO33_perturbations/ECCO33-14_Pu-239_coeff_0.300_MT18.pendf'
 
 f0 = open(pert0)
 lines0 = f0.readlines()
 sec0 = ENDF6.find_section(lines0, MF=3, MT=18)
 x0, y0 = ENDF6.read_table(sec0)
 
-f03 = open(pert03)
-lines03 = f03.readlines()
-sec03 = ENDF6.find_section(lines03, MF=3, MT=18)
-x03, y03 = ENDF6.read_table(sec03)
+fpert = open(pertgroup)
+linesp = fpert.readlines()
+secp = ENDF6.find_section(linesp, MF=3, MT=18)
+xp, yp = ENDF6.read_table(secp)
 
-f05 = open(pert_m05)
-lines05 = f05.readlines()
-sec05 = ENDF6.find_section(lines05, MF=3, MT=18)
-x05, y05 = ENDF6.read_table(sec05)
+# f03 = open(pert03)
+# lines03 = f03.readlines()
+# sec03 = ENDF6.find_section(lines03, MF=3, MT=18)
+# x03, y03 = ENDF6.read_table(sec03)
+#
+# f05 = open(pert_m05)
+# lines05 = f05.readlines()
+# sec05 = ENDF6.find_section(lines05, MF=3, MT=18)
+# x05, y05 = ENDF6.read_table(sec05)
 
 
 plt.figure()
 plt.plot(x0, y0, label='ENDF/B-VIII.0')
-plt.plot(x05, y05, label='-50% Perturbation')
-plt.plot(x03, y03, label = '+30% Perturbation')
+plt.plot(xp, yp, label = 'group perturbation')
+# plt.plot(x05, y05, label='-50% Perturbation')
+# plt.plot(x03, y03, label = '+30% Perturbation')
 plt.yscale('log')
 plt.xscale('log')
 plt.grid()
@@ -38,4 +44,5 @@ plt.xlabel('Energy / eV')
 plt.ylabel('$\sigma_{n,f}$')
 plt.title('Perturbed Pu-239 (n,f) cross sections')
 plt.legend()
+plt.savefig('Group_pert_test.png', dpi = 200)
 plt.show()
