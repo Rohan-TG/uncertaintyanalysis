db4f2833862dddefe7ad5ebcb0ba13df21d9ebe4
Rohan-TG <rohan.tg05@gmail.com>
2025-08-08 18:43:21 +0100

    test;

--- scone_benchmarks/flatp_mt18_Jezebel/p_1/read_output.py
+++ scone_benchmarks/flatp_mt18_Jezebel/p_1/read_output.py
@@ -1,17 +1,31 @@
 import os
 import pandas
+import tqdm
 
 dir = '/home/rnt26/PycharmProjects/uncertaintyanalysis/scone_benchmarks/flatp_mt18_Jezebel/p_1/outputfiles'
 
 files = os.listdir(dir)
 
-for filename in files:
+keff_list = []
+keff_error_list = []
+coeff_list = []
+
+for filename in tqdm.tqdm(files, total=len(files)):
 	obj = open(f'{dir}/{filename}')
 	if len(filename) == 14:
 		coefficient = float(filename[7:12])
+		coeff_list.append(coefficient)
 	elif len(filename) == 15:
 		coefficient = float(filename[7:13])
+		coeff_list.append(coefficient)
 
 	lines = obj.readlines()
 	keffline = lines[12]
-	break
+
+	keff_value_string = keffline[15:26]
+	keff_value_float = float(keff_value_string.replace('E', 'e'))
+	keff_list.append(keff_value_float)
+
+	keff_error = keffline[27:38]
+	keff_error_float = float(keff_error.replace('E', 'e'))
+	keff_error_list.append(keff_error_float)
