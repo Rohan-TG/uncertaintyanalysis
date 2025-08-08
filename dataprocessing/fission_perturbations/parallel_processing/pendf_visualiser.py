ddf2a2fbf975676a31ef53dc8a44e1e76e8ba3d6
Rohan-TG <rohan.tg05@gmail.com>
2025-08-08 16:49:17 +0100

    test

--- dataprocessing/fission_perturbations/parallel_processing/pendf_visualiser.py
+++ dataprocessing/fission_perturbations/parallel_processing/pendf_visualiser.py
@@ -1,21 +1,35 @@
 import matplotlib.pyplot as plt
 from dataprocessing import ENDF6
+import os
+import tqdm
 
 
-def plot_data(filename):
+# filename = 'Pu-239_coeff_0.154_MT18.pendf'
+# filename = 'Pu-239_coeff_0.015_MT18.pendf'
+#
+# f = open(filename)
+# lines = f.readlines()
+# section = ENDF6.find_section(lines, MF=3, MT=18)
+# erg, xs = ENDF6.read_table(section)
+#
+# plt.figure()
+# plt.plot(erg, xs, label = 'Data')
+# plt.legend()
+# plt.grid()
+# plt.xlabel('Energy / eV')
+# plt.ylabel('$\sigma_{n,f}$ / b')
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.title('Perturbed Pu-239 (n,f) cross sections')
+# plt.show()
 
-	f = open(filename)
+dir = '/home/rnt26/PycharmProjects/uncertaintyanalysis/dataprocessing/fission_perturbations/parallel_processing/pendf_perturbed'
+pendf_names = os.listdir(dir)
+
+length_list = []
+for file in tqdm.tqdm(pendf_names, total=len(pendf_names)):
+	f = open(file)
 	lines = f.readlines()
 	section = ENDF6.find_section(lines, MF=3, MT=18)
 	erg, xs = ENDF6.read_table(section)
-
-	plt.figure()
-	plt.plot(erg, xs, label = 'Data')
-	plt.legend()
-	plt.grid()
-	plt.xlabel('Energy / eV')
-	plt.ylabel('$\sigma_{n,f}$ / b')
-	plt.xscale('log')
-	plt.yscale('log')
-	plt.title('Perturbed Pu-239 (n,f) cross sections')
-	plt.show()
+	length_list.append(len(erg))
