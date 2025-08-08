5326f7b5ff57087fb4d2936e4e6afbf9dbeb2a3f
Rohan-TG <rohan.tg05@gmail.com>
2025-08-08 16:41:09 +0100

    running locally

--- dataprocessing/fission_perturbations/parallel_processing/pendf_visualiser.py
+++ dataprocessing/fission_perturbations/parallel_processing/pendf_visualiser.py
@@ -1,1 +1,19 @@
 import matplotlib.pyplot as plt
+from dataprocessing import ENDF6
+
+
+def plot_data(filename):
+
+	f = open(filename)
+	lines = f.readlines()
+	section = ENDF6.find_section(lines, MF=3, MT=18)
+	erg, xs = ENDF6.read_table(section)
+
+	plt.figure()
+	plt.plot(erg, xs, label = 'Data')
+	plt.legend()
+	plt.grid()
+	plt.xlabel('Energy / eV')
+	plt.ylabel('$\sigma_{n,f}$ / b')
+	plt.title('Perturbed Pu-239 (n,f) cross sections')
+	plt.show()
