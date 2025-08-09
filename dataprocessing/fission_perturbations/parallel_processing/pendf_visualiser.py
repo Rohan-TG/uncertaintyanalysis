413a0d11d12ad8d208a2e6aa347d02c91b8f7aa8
Rohan-TG <rohan.tg05@gmail.com>
2025-08-09 10:57:22 +0100

    asdf

--- dataprocessing/fission_perturbations/parallel_processing/pendf_visualiser.py
+++ dataprocessing/fission_perturbations/parallel_processing/pendf_visualiser.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+
 import ENDF6
 import os
 import tqdm
@@ -28,16 +30,30 @@ dir = '/home/rnt26/PycharmProjects/uncertaintyanalysis/dataprocessing/fission_pe
 pendf_names = os.listdir(dir)
 
 length_list = []
-def process_file(file):
-	f = open(f'{dir}/{file}')
+# def process_file(file):
+# 	f = open(f'{dir}/{file}')
+# 	lines = f.readlines()
+# 	section = ENDF6.find_section(lines, MF=3, MT=18)
+# 	erg, xs = ENDF6.read_table(section)
+# 	# return(len(erg))
+# 	# length_list.append(len(erg))
+#
+# # with ThreadPoolExecutor(max_workers=1) as executor:
+# 	futures = {executor.submit(process_file, file): file for file in pendf_names}
+# 	for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+# 		result = future.result()
+# 		length_list.append(result)
+
+
+for filename in tqdm.tqdm(pendf_names, total=len(pendf_names)):
+	f = open(f'{dir}/{filename}')
 	lines = f.readlines()
 	section = ENDF6.find_section(lines, MF=3, MT=18)
 	erg, xs = ENDF6.read_table(section)
-	return(len(erg))
-	# length_list.append(len(erg))
 
-with ThreadPoolExecutor(max_workers=1) as executor:
-	futures = {executor.submit(process_file, file): file for file in pendf_names}
-	for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
-		result = future.result()
-		length_list.append(result)
+	name_split = filename.split('_')
+	coefficient = float(name_split[2])
+	coeff_list = [coefficient for i in xs]
+
+	df_temp = pd.DataFrame({'ERG': erg, 'XS': xs, 'P':coeff_list})
+	df_temp.to_csv(f'Pu239_{coefficient}_flat_MT18_XS.csv')
