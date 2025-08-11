c429c686d475e9e6304367a66a1e4a4df6ffa36f
Rohan-TG <rohan.tg05@gmail.com>
2025-08-11 11:49:48 +0100

    test

--- dataprocessing/fission_perturbations/parallel_processing/data_maker.py
+++ dataprocessing/fission_perturbations/parallel_processing/data_maker.py
@@ -8,11 +8,17 @@ keff_data = pd.read_csv('raw_keff_data.csv')
 
 xscsv_filenames = os.listdir(xscsv_dir)
 
+
+
+
 for xsfile in tqdm.tqdm(xscsv_filenames, total=len(xscsv_filenames)):
 	df = pd.read_csv(f'{xscsv_dir}/{xsfile}')
 	coeff = df['P'].values[0]
 
-	reduced_keff_df = keff_data[keff_data.p == coeff]
-	print(reduced_keff_df)
-	print(coeff)
-	break
+	keff_p_values = keff_data['p'].values
+	if coeff in keff_p_values:
+
+		reduced_keff_df = keff_data[keff_data.p == coeff]
+		print(reduced_keff_df)
+		print(coeff)
+		break
