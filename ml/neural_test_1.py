183581a7721095fc7e619bb5d9fd0d98b8bfdca3
Rohan-TG <rohan.tg05@gmail.com>
2025-08-14 22:27:45 +0100

    run tests

--- ml/neural_test_1.py
+++ ml/neural_test_1.py
@@ -5,11 +5,25 @@ import numpy
 import sklearn.preprocessing
 import matplotlib.pyplot as plt
 import os
+import random
 
 
 data_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata'
 
 all_csvs = os.listdir(data_directory)
+
+training_csvs = []
+while len(training_csvs) < 290:
+	choice = random.choice(all_csvs)
+	if choice not in training_csvs:
+		training_csvs.append(choice)
+
+
+X_train = []
+y_train = []
+for file in training_csvs:
+	df = pd.read_csv(file)
+	break
 
 
 
