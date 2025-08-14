d4d7d66df97e8d106de6d7da4772b0fe1a40e3c2
Rohan-TG <rohan.tg05@gmail.com>
2025-08-14 22:03:15 +0100

    run now

--- dataprocessing/fission_perturbations/parallel_processing/datatruncate.py
+++ dataprocessing/fission_perturbations/parallel_processing/datatruncate.py
@@ -1,1 +1,19 @@
 import pandas as pd
+import os
+import tqdm
+import subprocess
+
+alldata_dir = '/home/rnt26/PycharmProjects/uncertaintyanalysis/dataprocessing/fission_perturbations/parallel_processing/fulldata'
+
+pruned_ml_dir = '/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata'
+
+csv_filenames = os.listdir(alldata_dir)
+
+for csv in tqdm.tqdm(csv_filenames, total=len(csv_filenames)):
+
+	df = pd.read_csv(csv)
+
+	current_keff = df['keff'].values[0]
+
+	if current_keff >= 0.89 and current_keff <= 1.11:
+		subprocess.run(f'cp {alldata_dir}/{csv} {pruned_ml_dir}')
