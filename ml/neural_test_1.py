a4e420099ac824603236c417ad1d3938a06a80f4
Rohan-TG <rohan.tg05@gmail.com>
2025-08-14 22:00:46 +0100

    blue

--- ml/neural_test_1.py
+++ ml/neural_test_1.py
@@ -1,1 +1,13 @@
 import pandas as pd
+import tensorflow as tf
+import keras
+import numpy
+import sklearn.preprocessing
+import matplotlib.pyplot as plt
+
+
+
+
+model =keras.Sequential()
+model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+
