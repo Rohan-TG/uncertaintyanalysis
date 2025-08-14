6726fe278bb26fd38ef012dff526d09eb398bf20
Rohan-TG <rohan.tg05@gmail.com>
2025-08-14 22:22:00 +0100

    test

--- ml/neural_test_1.py
+++ ml/neural_test_1.py
@@ -4,10 +4,22 @@ import keras
 import numpy
 import sklearn.preprocessing
 import matplotlib.pyplot as plt
+import os
+
+
+data_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata'
+
+all_csvs = os.listdir(data_directory)
 
 
 
 
-model =keras.Sequential()
-model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+
+
+
+
+
+
+# model =keras.Sequential()
+# model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
 
