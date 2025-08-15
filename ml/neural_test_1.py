98c016c23d86d610ad835aecbfb6e01b2a21be57
Rohan-TG <rohan.tg05@gmail.com>
2025-08-15 16:07:17 +0100

    test data

--- ml/neural_test_1.py
+++ ml/neural_test_1.py
@@ -20,6 +20,10 @@ while len(training_csvs) < 290:
 	if choice not in training_csvs:
 		training_csvs.append(choice)
 
+test_csvs = []
+for csv in all_csvs:
+	if csv not in training_csvs:
+		test_csvs.append(csv)
 
 XS_train = []
 y_train = []
@@ -46,6 +50,25 @@ X_train = scaled_columns_xtrain.transpose()
 
 
 
+XS_test = []
+y_test = []
+for file in tqdm.tqdm(test_csvs, total=len(test_csvs)):
+	dftest = pd.read_csv(f'{data_directory}/{file}')
+	y_test += [float(dftest['keff'].values[0])]
+	XS_test.append(dftest['XS'].values)
+
+XS_test = np.array(XS_test)
+y_test = zscore(y_test)
+
+scaling_matrix_xtest = XS_test.transpose()
+
+scaled_columns_xtest = []
+for column in tqdm.tqdm(scaling_matrix_xtest[1:], total=len(scaling_matrix_xtest[1:])):
+	scaled_column = zscore(column)
+	scaled_columns_xtest.append(scaled_column)
+
+scaled_columns_xtest = np.array(scaled_columns_xtest)
+X_test = scaled_columns_xtest.transpose()
 
 
 
@@ -54,6 +77,23 @@ X_train = scaled_columns_xtrain.transpose()
 
 
 
-# model =keras.Sequential()
-# model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
 
+
+model =keras.Sequential()
+model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+model.add(keras.layers.Dense(200, activation='relu'))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(1, activation='linear'))
+model.compile(loss='mean_absolute_error', optimizer='adam')
+
+history = model.fit(X_train,
+					y_train,
+					epochs=20,
+					batch_size=16,
+					# callbacks=callback,
+					validation_data=(X_test, y_test),
+					verbose=1)
+
+
+predictions = model.predict(X_test)
+predictions = predictions.ravel()
