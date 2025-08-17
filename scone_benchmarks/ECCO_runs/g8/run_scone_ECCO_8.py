72a4b2901e553b4f1d54f847124b2d1d2211076c
Rohan-TG <rohan.tg05@gmail.com>
2025-08-17 23:04:46 +0100

    15 cores

--- scone_benchmarks/ECCO_runs/g8/run_scone_ECCO_8.py
+++ scone_benchmarks/ECCO_runs/g8/run_scone_ECCO_8.py
@@ -10,11 +10,11 @@ ACE_file_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/data/ecco8
 
 scone_executable_path = '/home/rnt26/scone/SCONE/Build/scone.out' # location of the scone executable
 
-num_cores = 20 # number of cores to use for this specific instance of scone
-# perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
+num_cores = 15 # number of cores to use for this specific instance of scone
+perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
 
 
-perturbation_coefficients = [-0.499, 0.000, 0.500]
+# perturbation_coefficients = [-0.499, 0.000, 0.500]
 
 for coefficient in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coefficients)):
 	libfile = 'lib8.xsfile'
