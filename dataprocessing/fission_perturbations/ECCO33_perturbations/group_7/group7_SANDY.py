6e5821d3c8ce4e332f2fc2f5dcf06cdcbcbbafbe
Rohan-TG <rohan.tg05@gmail.com>
2025-08-21 12:49:16 +0100

    g7

--- dataprocessing/fission_perturbations/ECCO33_perturbations/group_7/group7_SANDY.py
+++ dataprocessing/fission_perturbations/ECCO33_perturbations/group_7/group7_SANDY.py
@@ -19,9 +19,10 @@ perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
 
 endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
 pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
-pendf = endf6.get_pendf(err=0.0001, verbose=True)
+# pendf = endf6.get_pendf(err=0.0001, verbose=True)
 
-xs = sandy.Xs.from_endf6(pendf)
+# xs = sandy.Xs.from_endf6(pendf)
+heated_xs = sandy.Xs.from_endf6(pendfheated)
 
 # 6	8.2085000000E-01
 # 7	4.9787070000E-01
@@ -39,19 +40,20 @@ for coeff in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coeffic
 
     perturbation = sandy.Pert([1, 1 + coeff], index=domain)
 
-    xspert = xs.custom_perturbation(mat, mt, perturbation)
+    # xspert = xs.custom_perturbation(mat, mt, perturbation)
+    heated_xspert = heated_xs.custom_perturbation(mat, mt, perturbation)
 
-    pendf_pert = xspert.to_endf6(pendf) # Create PENDF of perturbed data
-    heated_pendf_pert = xspert.to_endf6(pendfheated)
+    # pendf_pert = xspert.to_endf6(pendf) # Create PENDF of perturbed data
+    heated_pendf_pert = heated_xspert.to_endf6(pendfheated)
 
-    tag = "_pert"
-    outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
+    # tag = "_pert"
+    # outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
 
-    savefilename = f"ECCO33-g14_Pu9_{coeff:0.3f}_MT18.09c"
-    with open(f"{savefilename}", mode="w") as f:
-        f.write(outs["ace"])
+    # savefilename = f"ECCO33-g2_Pu9_{coeff:0.3f}_MT18.09c"
+    # with open(f"{savefilename}", mode="w") as f:
+    #     f.write(outs["ace"])
 
-    savefilependf = f"ECCO33-g14_Pu9_{coeff:0.3f}_MT18.pendf"
+    savefilependf = f"ECCO33-g7_Pu9_{coeff:0.3f}_MT18.pendf"
     heated_pendf_pert.to_file(savefilependf)
 
 
