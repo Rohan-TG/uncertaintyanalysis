610180869352673d4d540fccc7fd2c3ca587c190
Rohan-TG <rohan.tg05@gmail.com>
2025-08-23 12:39:48 +0100

    removed pendf generation temporarily awaiting fix

--- dataprocessing/capture/flat_ngamma/tempnegative/flat_negative_ng.py
+++ dataprocessing/capture/flat_ngamma/tempnegative/flat_negative_ng.py
@@ -13,12 +13,12 @@ start = time.time()
 za = 94239
 
 
-perturbation_coefficients = np.arange(-0.500, 0.000, 0.001)
+perturbation_coefficients = np.arange(-0.011, 0.000, 0.001)
 
 # perturbation_coefficients = [-0.3, -0.1, 0.1, 0.2, 0.3, 0.4]
 
 endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
-pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
+# pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
 pendf = endf6.get_pendf(err=0.0001, verbose=True)
 
 xs = sandy.Xs.from_endf6(pendf)
@@ -39,7 +39,7 @@ for coeff in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coeffic
     xspert = xs.custom_perturbation(mat, mt, perturbation)
 
     pendf_pert = xspert.to_endf6(pendf) # Create PENDF of perturbed data
-    heated_pendf_pert = xspert.to_endf6(pendfheated)
+    # heated_pendf_pert = xspert.to_endf6(pendfheated)
 
     tag = "_pert"
     outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
@@ -48,8 +48,8 @@ for coeff in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coeffic
     with open(f"{savefilename}", mode="w") as f:
         f.write(outs["ace"])
 
-    savefilependf = f"flat_Pu9_{coeff:0.3f}_MT102.pendf"
-    heated_pendf_pert.to_file(savefilependf)
+    # savefilependf = f"flat_Pu9_{coeff:0.3f}_MT102.pendf"
+    # heated_pendf_pert.to_file(savefilependf)
 
 
 
