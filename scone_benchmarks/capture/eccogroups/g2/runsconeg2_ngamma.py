becd006a373280a79889882e0d3d086766412293
Rohan-TG <rohan.tg05@gmail.com>
2025-08-26 16:12:58 +0100

    run

---
+++ scone_benchmarks/capture/eccogroups/g2/runsconeg2_ngamma.py
@@ -0,0 +1,51 @@
+import numpy as np
+import tqdm
+import time
+import subprocess
+import datetime
+import os
+
+start_time = time.time()
+
+ACE_file_directory = '/home/rnt26/uncertaintyanalysis/data/ng/g2' # location of the ECCO Group 6 perturbed ACE files to be used for generating samples
+
+scone_executable_path = '/home/rnt26/scone/SCONE/Build/scone.out' # location of the scone executable
+
+num_cores = 20 # number of cores to use for this specific instance of scone
+perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
+
+search_files = os.listdir()
+for file in search_files:
+	if '.xsfile' in file:
+		libfile = file
+		break
+# perturbation_coefficients = [-0.499, 0.000, 0.500]
+
+xsfile_fullpath = os.path.abspath(libfile)
+os.environ["SCONE_ACE"] = xsfile_fullpath
+
+for coefficient in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coefficients)):
+
+	input_coefficient = round(coefficient, 3) # Coefficient string prep
+
+	ACE_filename = f"{ACE_file_directory}/ECCO33-g2_Pu9_{input_coefficient:0.3f}_MT102.09c" # name of ACE file for this SCONE run
+
+	with open(libfile, 'r') as file:
+		lines = file.readlines()
+
+	Pu_239_address = 511 # Line number of the Pu-239 ACE file address
+
+	lines[Pu_239_address] = f'94239.00c; 1; {ACE_filename};\n' # Rewrite line
+
+	with open(libfile, 'w') as file: # write to new lib1.xsfile
+		file.writelines(lines)
+
+	subprocess.run(f'{scone_executable_path} --omp {num_cores} Jezebel', shell=True) # run scone
+
+	subprocess.run(f'mv output.m outputfiles/output-{input_coefficient:0.3f}.m', shell=True) # move output file to output directory for later analysis
+
+
+
+end_time = time.time()
+
+print(f"Time elapsed: {datetime.timedelta(seconds=end_time-start_time)}")
