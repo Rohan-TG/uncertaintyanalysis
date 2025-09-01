f50d9f638b2e3ad11ec03df21538f4099e88ab07
Rohan-TG <rohan.tg05@gmail.com>
2025-09-01 13:11:26 +0100

    ready

--- scone_benchmarks/ECCO_runs/g30_33/run_scone_g30-33.py
+++ scone_benchmarks/ECCO_runs/g30_33/run_scone_g30-33.py
@@ -7,15 +7,28 @@ import os
 
 start_time = time.time()
 
-ACE_file_directory = '/home/rnt26/uncertaintyanalysis/data/ecco3033Pu9/' # location of the ECCO Group 5 perturbed ACE files to be used for generating samples
+ACE_file_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/data/g2933/mt18' # location of the ECCO Group 6 perturbed ACE files to be used for generating samples
 
 scone_executable_path = '/home/rnt26/scone/SCONE/Build/scone.out' # location of the scone executable
 
-num_cores = 20 # number of cores to use for this specific instance of scone
+
 perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
 
+ZA = 94239
 
-# perturbation_coefficients = [-0.499, 0.000, 0.500]
+default_xsfile = 'endfb-viii0.xsfile'
+current_dir = os.getcwd()
+target_dir = 'scone_benchmarks'
+idx = current_dir.find(target_dir)
+default_Jezebel = 'Jezebel'
+
+target_path = current_dir[:(idx + len(target_dir))]
+libfilename = f'lib{ZA}.xsfile'
+
+
+subprocess.run(f"cp {target_path}/{default_xsfile} {libfilename}", shell=True)
+subprocess.run(f"cp {target_path}/{default_Jezebel} Jezebel", shell=True)
+
 
 
 search_files = os.listdir()
@@ -27,7 +40,16 @@ for file in search_files:
 xsfile_fullpath = os.path.abspath(libfile)
 os.environ["SCONE_ACE"] = xsfile_fullpath
 
+if 'outputfiles' not in search_files:
+	subprocess.run('mkdir outputfiles', shell=True)
+
+
+
+num_cores = int(input('Core no.: '))# number of cores to use for this specific instance of scone
+
+
 for coefficient in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coefficients)):
+	# subprocess.run('echo $SCONE_ACE', shell=True)
 
 	input_coefficient = round(coefficient, 3) # Coefficient string prep
 
@@ -36,9 +58,12 @@ for coefficient in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_c
 	with open(libfile, 'r') as file:
 		lines = file.readlines()
 
-	Pu_239_address = 511 # Line number of the Pu-239 ACE file address
+	for i, line in enumerate(lines): # Find location of the nuclide in the .xsfile
+		if str(ZA) in line:
+			nuclide_ACE_address = i
+			break
 
-	lines[Pu_239_address] = f'94239.00c; 1; {ACE_filename};\n' # Rewrite line
+	lines[nuclide_ACE_address] = f'{ZA}.00c; 1; {ACE_filename};\n' # Rewrite line
 
 	with open(libfile, 'w') as file: # write to new lib1.xsfile
 		file.writelines(lines)
