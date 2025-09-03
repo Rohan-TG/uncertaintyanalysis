026cb329a2d683b6dd31ec9d34fc5c39f654f184
Rohan-TG <rohan.tg05@gmail.com>
2025-09-03 15:54:15 +0100

    ready

--- dataprocessing/n2n/Pu239/groups/g3/g3_pu9_n2n_sandy.py
+++ dataprocessing/n2n/Pu239/groups/g3/g3_pu9_n2n_sandy.py
@@ -18,11 +18,11 @@ perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
 # perturbation_coefficients = [-0.5, 0.5]
 
 endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
-pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
+# pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
 pendf = endf6.get_pendf(err=0.0001, verbose=True)
 
 xs = sandy.Xs.from_endf6(pendf)
-heated_xs = sandy.Xs.from_endf6(pendfheated)
+# heated_xs = sandy.Xs.from_endf6(pendfheated)
 
 lower_bound = 3.6787940000e6  # group 3 eV
 upper_bound = 6.0653070000e6   # group 2 eV
@@ -38,10 +38,10 @@ for coeff in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coeffic
     perturbation = sandy.Pert([1, 1 + coeff], index=domain)
 
     xspert = xs.custom_perturbation(mat, mt, perturbation)
-    heated_xspert = heated_xs.custom_perturbation(mat, mt, perturbation)
+    # heated_xspert = heated_xs.custom_perturbation(mat, mt, perturbation)
 
     pendf_pert = xspert.to_endf6(pendf) # Create PENDF of perturbed data
-    heated_pendf_pert = heated_xspert.to_endf6(pendfheated)
+    # heated_pendf_pert = heated_xspert.to_endf6(pendfheated)
 
     tag = "_pert"
     outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
@@ -50,8 +50,8 @@ for coeff in tqdm.tqdm(perturbation_coefficients, total=len(perturbation_coeffic
     with open(f"{savefilename}", mode="w") as f:
         f.write(outs["ace"])
 
-    savefilependf = f"Pu9_g3_{coeff:0.3f}_MT16.pendf"
-    heated_pendf_pert.to_file(savefilependf)
+    # savefilependf = f"Pu9_g3_{coeff:0.3f}_MT16.pendf"
+    # heated_pendf_pert.to_file(savefilependf)
 
 
 
