e77cb4ce5db7cd61fb029742da8714ec9cfba98b
Rohan-TG <rohan.tg05@gmail.com>
2025-09-04 17:05:41 +0100

    first attempt

--- dataprocessing/fission_perturbations/Pu-239-multigroup-perturbations/g1_g4/sim_perturbation_g1_g4_sandy.py
+++ dataprocessing/fission_perturbations/Pu-239-multigroup-perturbations/g1_g4/sim_perturbation_g1_g4_sandy.py
@@ -1,8 +1,11 @@
 import sandy
 import datetime
-# import numpy as np
+import numpy as np
 import time
 import sys
+
+import tqdm
+
 sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis')
 
 
@@ -11,7 +14,8 @@ from groupEnergies import Groups
 start = time.time()
 za = 94239
 
-perturbation_coefficient1 = -0.95
+perturbation_domain = np.arange(-25, 27, 2)
+
 
 endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
 pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
@@ -24,53 +28,52 @@ first_group_lower_bound = Groups.g1  # Group 1 eV
 first_group_upper_bound = Groups.g0   # Group 0 eV
 first_group_domain = [first_group_lower_bound, first_group_upper_bound]
 
+secondary_lower_bound = Groups.g4  # group 4
+secondary_upper_bound = Groups.g3  # group 3
+secondary_domain = [secondary_lower_bound, secondary_upper_bound]
+
 mat = 9437
 mt = 18
 
-perturbation = sandy.Pert([1, 1 + perturbation_coefficient1], index=first_group_domain)
-xspert = xs_unheated.custom_perturbation(mat, mt, perturbation)
-xspert_heated = xs_heated.custom_perturbation(mat, mt, perturbation)
 
-pendf_pert = xspert.to_endf6(original_pendf) # The perturbed file, time to perturb it again
-heated_pendf_pert = xspert_heated.to_endf6(pendfheated)
+for pg1 in tqdm.tqdm(perturbation_domain, total=len(perturbation_domain)):
+	for pg2 in perturbation_domain:
 
-# tag = "_pert"
-# outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
-# savefilename = f"regular_single_perturbation_ace.09c"
-# with open(f"{savefilename}", mode="w") as f:
-# 	f.write(outs["ace"])
+		perturbation = sandy.Pert([1, 1 + pg1], index=first_group_domain)
+		xspert = xs_unheated.custom_perturbation(mat, mt, perturbation)
+		xspert_heated = xs_heated.custom_perturbation(mat, mt, perturbation)
 
-# heated_pendf_pert.to_file('single-perturbation-file-3.pendf')
+		pendf_pert = xspert.to_endf6(original_pendf) # The perturbed file, time to perturb it again
+		heated_pendf_pert = xspert_heated.to_endf6(pendfheated)
 
+		# tag = "_pert"
+		# outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf_pert)
+		# savefilename = f"regular_single_perturbation_ace.09c"
+		# with open(f"{savefilename}", mode="w") as f:
+		# 	f.write(outs["ace"])
 
-secondary_lower_bound = Groups.g4 # group 4
-secondary_upper_bound = Groups.g3 # group 3
-secondary_domain = [secondary_lower_bound, secondary_upper_bound]
-
-secondary_coefficient = 3
+		# heated_pendf_pert.to_file('single-perturbation-file-3.pendf')
 
 
+		###########################################################################################################################
+		# begin secondary perturbation
+		xs_2_unheated = sandy.Xs.from_endf6(pendf_pert)
+		xs_2_heated = sandy.Xs.from_endf6(heated_pendf_pert)
 
+		secondary_perturbation = sandy.Pert([1, 1+ pg2], index=secondary_domain)
+		xspert_2_unheated = xs_2_unheated.custom_perturbation(mat, mt, secondary_perturbation)
 
-###########################################################################################################################
-# begin secondary perturbation
-xs_2_unheated = sandy.Xs.from_endf6(pendf_pert)
-xs_2_heated = sandy.Xs.from_endf6(heated_pendf_pert)
+		xspert_2_heated = xs_2_heated.custom_perturbation(mat, mt, secondary_perturbation)
 
-secondary_perturbation = sandy.Pert([1, 1+ secondary_coefficient], index=secondary_domain)
-xspert_2_unheated = xs_2_unheated.custom_perturbation(mat, mt, secondary_perturbation)
+		secondary_pendf_pert = xspert_2_unheated.to_endf6(pendf_pert)
+		secondary_heated_pendf_pert = xspert_2_heated.to_endf6(heated_pendf_pert)
 
-xspert_2_heated = xs_2_heated.custom_perturbation(mat, mt, secondary_perturbation)
+		secondary_heated_pendf_pert.to_file(f'Pu9_dual_g1_{pg1}-g4_{pg2}_MT18.pendf')
 
-secondary_pendf_pert = xspert_2_unheated.to_endf6(pendf_pert)
-secondary_heated_pendf_pert = xspert_2_heated.to_endf6(heated_pendf_pert)
-
-secondary_heated_pendf_pert.to_file('simtest-trial-g1g4.pendf')
-
-secondary_outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=secondary_pendf_pert)
-savefilename2 = f"dual-perturbation-ace-g1-g4.09c"
-with open(f"{savefilename2}", mode="w") as f:
-	f.write(secondary_outs["ace"])
+		secondary_outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=secondary_pendf_pert)
+		savefilename2 = f"Pu9_dual_g1_{pg1}-g4_{pg2}_MT18.09c"
+		with open(f"{savefilename2}", mode="w") as f:
+			f.write(secondary_outs["ace"])
 
 
 
