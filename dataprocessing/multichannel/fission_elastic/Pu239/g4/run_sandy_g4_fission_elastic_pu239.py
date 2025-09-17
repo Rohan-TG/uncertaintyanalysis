f6dcd64ad2a3ec6f186826be914e4989afe06f9c
Rohan-TG <rohan.tg05@gmail.com>
2025-09-17 21:38:30 +0100

    run on fermiac instead

--- dataprocessing/multichannel/fission_elastic/Pu239/g4/run_sandy_g4_fission_elastic_pu239.py
+++ dataprocessing/multichannel/fission_elastic/Pu239/g4/run_sandy_g4_fission_elastic_pu239.py
@@ -1,7 +1,7 @@
 import sandy
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import sys
-sys.path.append('/home/rnt26/uncertaintyanalysis')
+sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis')
 from groupEnergies import Pu239, Groups, Reactions
 import tqdm
 import time
