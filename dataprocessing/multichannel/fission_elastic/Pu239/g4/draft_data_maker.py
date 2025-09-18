c073723891792a8c3715c50853d79a38e65b8913
Rohan-TG <rohan.tg05@gmail.com>
2025-09-18 20:39:56 +0100

    temporary, wip

---
+++ dataprocessing/multichannel/fission_elastic/Pu239/g4/draft_data_maker.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import tqdm
+import ENDF6
+import os
+
+keffs = pd.read_csv('g1_Pu-239_MT18_keffs.csv')
+
+pendf_dir = '/home/rnt26/uncertaintyanalysis/dataprocessing/fission_perturbations/ECCO33_perturbations/group_1/pendf/'
+
+pendf_names = os.listdir(pendf_dir)
+length_list = []
+
+MT = 18
+
+for filename in tqdm.tqdm(pendf_names, total=len(pendf_names)):
+	f = open(f'{pendf_dir}/{filename}')
+	lines = f.readlines()
+	section = ENDF6.find_section(lines, MF=3, MT=MT)
+	erg, xs = ENDF6.read_table(section)
+
+	name_split = filename.split('_')
+	coefficient = float(name_split[2])
+	coeff_list = [coefficient for i in xs]
+
+	reduced_keff_df = keffs[keffs.p == coefficient]
+	keff_list = [reduced_keff_df['keff'].values[0] for i in xs]
+	keff_err_list = [reduced_keff_df['keff_err'].values[0] for i in xs]
+
+	df = pd.DataFrame({'ERG': erg,
+					   'XS': xs,
+					   'keff': keff_list,
+					   'keff_err': keff_err_list,
+					   'p': coeff_list})
+
+	df.to_csv(f'csvs/g1_Pu9_{coefficient:0.3f}_MT18.csv')
+	# df_temp = pd.DataFrame({'ERG': erg, 'XS': xs, 'P':coeff_list})
+
+
+	# df_temp.to_csv(f'Pu239_{coefficient}_flat_MT18_XS.csv')
