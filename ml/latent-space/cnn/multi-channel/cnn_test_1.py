598e0d0df894376e1602e7620976512f8eaa69f3
Rohan-TG <rohan.tg05@gmail.com>
2025-09-19 16:57:36 +0100

    data processing

--- ml/latent-space/cnn/multi-channel/cnn_test_1.py
+++ ml/latent-space/cnn/multi-channel/cnn_test_1.py
@@ -9,4 +9,20 @@ import tqdm
 import matplotlib.pyplot as plt
 
 
-data_directory =
+data_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata/g4_fission_elastic_pu9'
+
+all_parquets = os.listdir(data_directory)
+
+n_training_samples = int(0.9 * len(all_parquets))
+
+training_files = []
+while len(training_files) < n_training_samples:
+	choice = random.choice(all_parquets)
+	if choice not in training_files:
+		training_files.append(choice)
+
+test_csvs = []
+for csv in all_parquets:
+	if csv not in training_files:
+		test_csvs.append(csv)
+
