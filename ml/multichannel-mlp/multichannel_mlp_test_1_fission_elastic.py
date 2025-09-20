ef813837a417293f97ad10c0dc988e2fe9680363
Rohan-TG <rohan.tg05@gmail.com>
2025-09-20 18:34:53 +0100

    small network

--- ml/multichannel-mlp/multichannel_mlp_test_1_fission_elastic.py
+++ ml/multichannel-mlp/multichannel_mlp_test_1_fission_elastic.py
@@ -132,8 +132,8 @@ callback = keras.callbacks.EarlyStopping(monitor='val_loss',
 										 restore_best_weights=True)
 
 model =keras.Sequential()
-model.add(keras.layers.Dense(50, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
-model.add(keras.layers.Dense(30, activation='relu'))
+model.add(keras.layers.Dense(10, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+model.add(keras.layers.Dense(10, activation='relu'))
 # model.add(keras.layers.Dense(100, activation='relu'))
 # model.add(keras.layers.Dense(1000, activation='relu'))
 # model.add(keras.layers.Dense(500, activation='relu'))
