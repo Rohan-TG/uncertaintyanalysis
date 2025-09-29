f12256899a4048527dbc026790518b780d343be9
Rohan-TG <rohan.tg05@gmail.com>
2025-09-29 12:04:13 +0100

    attempting url fetch again

--- dataprocessing/Pu240/capture/groups/g20_33/run_sandy_g20-33_pu240_capture.py
+++ dataprocessing/Pu240/capture/groups/g20_33/run_sandy_g20-33_pu240_capture.py
@@ -19,7 +19,8 @@ group = '2033'
 
 perturbation_coefficients = np.arange(-0.500, 0.501, 0.001)
 
-endf6 = sandy.Endf6.from_file('/home/rnt26/uncertaintyanalysis/n-094_Pu_240.endf')    # sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
+# endf6 = sandy.Endf6.from_file('/home/rnt26/uncertaintyanalysis/n-094_Pu_240.endf')
+endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
 pendfheated = endf6.get_pendf(err=0.0001, verbose=True, temperature=300)
 pendf = endf6.get_pendf(err=0.0001, verbose=True)
 
