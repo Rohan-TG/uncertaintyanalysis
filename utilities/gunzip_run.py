7221983bdd792b8d05530c2e87ddd7e45781b79a
Rohan-TG <rohan.tg05@gmail.com>
2025-10-10 13:05:32 +0100

    unzip script

---
+++ utilities/gunzip_run.py
@@ -0,0 +1,36 @@
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import tqdm
+import subprocess
+import time
+import datetime
+
+start = time.time()
+
+target_directory = input("Enter directory: ")
+filetype = input("Enter file type: ")
+processes = int(input("Enter n. processes: "))
+cores_per_job = 1
+
+all_files = os.listdir(target_directory)
+
+filenames = []
+for x in all_files:
+	if filetype in x:
+		filenames.append(x)
+
+def run_xz(file):
+	"""Runs unxz on one file"""
+	subprocess.run(f'gunzip {target_directory}/{file}', shell=True)
+
+with ProcessPoolExecutor(max_workers=processes) as executor:
+	futures = [executor.submit(run_xz, f) for f in filenames]
+
+	for i in tqdm.tqdm(as_completed(futures), total=len(futures)):
+		pass
+
+end = time.time()
+
+elapsed = end - start
+print(f"Time elapsed: {datetime.timedelta(seconds=elapsed)}")
+
