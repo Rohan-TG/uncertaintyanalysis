13fc6184060abc91aa57790daf634792623b55ac
Rohan-TG <rohan.tg05@gmail.com>
2025-10-15 13:11:27 +0100

    testing ml

--- ml/multichannel-mlp/test_morris.py
+++ ml/multichannel-mlp/test_morris.py
@@ -179,3 +179,61 @@ X_test = np.array(X_test)
 X_test = X_test.reshape([X_test.shape[0], X_test.shape[2], X_test.shape[1]])
 print('Test data processed...')
 
+callback = keras.callbacks.EarlyStopping(monitor='val_loss',
+										 # min_delta=0.005,
+										 patience=20,
+										 mode='min',
+										 start_from_epoch=5,
+										 restore_best_weights=True)
+
+model = keras.Sequential()
+model.add(keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2])))
+model.add(keras.layers.Conv1D(filters=16, kernel_size=3, padding='same', activation='relu',))
+model.add(keras.layers.Flatten())
+# model.add(keras.layers.Dense(10, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+model.add(keras.layers.Dense(10, activation='relu'))
+model.add(keras.layers.Dense(1, activation='linear'))
+# # model.add(keras.layers.Dense(1000, activation='relu'))
+# # model.add(keras.layers.Dense(500, activation='relu'))
+# model.add(keras.layers.Dense(1, activation='linear'))
+model.compile(loss='MeanSquaredError', optimizer='adam')
+
+
+trainstart = time.time()
+history = model.fit(X_train,
+					y_train,
+					epochs=100,
+					batch_size=4,
+					callbacks=callback,
+					validation_data=(X_test, y_test),
+					verbose=1)
+
+
+train_end = time.time()
+print(f'Training completed in {datetime.timedelta(seconds=(train_end - trainstart))}')
+predictions = model.predict(X_test)
+predictions = predictions.ravel()
+
+
+
+rescaled_predictions = []
+predictions_list = predictions.tolist()
+
+for pred in predictions_list:
+	descaled_p = pred * keff_std + keff_mean
+	rescaled_predictions.append(float(descaled_p))
+
+errors = []
+for predicted, true in zip(rescaled_predictions, keff_test):
+	errors.append((predicted - true) * 1e5)
+	print(f'SCONE: {true:0.5f} - ML: {predicted:0.5f}, Difference = {(predicted - true) * 1e5:0.0f} pcm')
+
+
+sorted_errors = sorted(errors)
+absolute_errors = [abs(x) for x in sorted_errors]
+print(f'Average absolute error: {np.mean(absolute_errors)} +- {np.std(absolute_errors)}')
+
+print(f'Max -ve error: {sorted_errors[0]} pcm, Max +ve error: {sorted_errors[-1]} pcm')
+
+
+print(f"Smallest absolute error: {min(absolute_errors)} pcm")
