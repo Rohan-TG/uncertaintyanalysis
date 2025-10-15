015bac70d1efbdfe26ea10994b6d944095a3309f
Rohan-TG <rohan.tg05@gmail.com>
2025-10-15 13:09:42 +0100

    testing test data

--- ml/multichannel-mlp/test_morris.py
+++ ml/multichannel-mlp/test_morris.py
@@ -103,37 +103,79 @@ keff_train_std = np.std(keff_train)
 
 
 
-# def scaler(channel_matrix):
-#
-# 	transposed_matrix = np.transpose(np.array(channel_matrix))
-# 	scaled_rows = []
-# 	for row in tqdm.tqdm(transposed_matrix, total=len(transposed_matrix)):
-# 		scaled_row = zscore(row)
-# 		scaled_rows.append(scaled_row)
-#
-# 	scaled_rows = np.array(scaled_rows)
-# 	transposed_scaled_matrix = np.transpose(scaled_rows)
-#
-# 	final_matrix = []
-# 	for i in transposed_scaled_matrix:
-# 		row = i[~np.isnan(i)]
-# 		final_matrix.append(row)
-#
-# 	final_matrix = np.array(final_matrix)
-# 	return final_matrix
-#
-#
-#
-# scaled_fission_train = scaler(fission_train)
-# scaled_elastic_train = scaler(elastic_train)
-#
-#
-# X_train = []
-# for f_data, e_data in zip(scaled_fission_train, scaled_elastic_train):
-# 	X_train.append([f_data, e_data])
-#
-# X_train = np.array(X_train)
-# X_train = X_train.reshape([X_train.shape[0], X_train.shape[2], X_train.shape[1]])
-#
-#
-# print('Training data processed...')
+def scaler(channel_matrix):
+
+	transposed_matrix = np.transpose(np.array(channel_matrix))
+	scaled_rows = []
+	for row in tqdm.tqdm(transposed_matrix, total=len(transposed_matrix)):
+		scaled_row = zscore(row)
+		scaled_rows.append(scaled_row)
+
+	scaled_rows = np.array(scaled_rows)
+	transposed_scaled_matrix = np.transpose(scaled_rows)
+
+	final_matrix = []
+	for i in transposed_scaled_matrix:
+		row = i[~np.isnan(i)]
+		final_matrix.append(row)
+
+	final_matrix = np.array(final_matrix)
+	return final_matrix
+
+
+
+scaled_fission_train = scaler(fission_train)
+scaled_elastic_train = scaler(elastic_train)
+
+
+X_train = []
+for f_data, e_data in zip(scaled_fission_train, scaled_elastic_train):
+	X_train.append([f_data, e_data])
+
+X_train = np.array(X_train)
+X_train = X_train.reshape([X_train.shape[0], X_train.shape[2], X_train.shape[1]])
+
+
+print('Training data processed...')
+
+
+
+
+
+
+########################################## Test data preparation #######################################################
+keff_test = []
+
+fission_test = []
+elastic_test = []
+for testfile in tqdm.tqdm(test_files, total=len(test_files)):
+	dftest = pd.read_parquet(f'{test_data_directory}/{testfile}', engine='pyarrow')
+	keff_test += [float(dftest['keff'].values[0])]
+
+	dftest = dftest[dftest.ERG >= g4boundary]
+	dftest = dftest[dftest.ERG <= g3boundary]
+
+	mt18xstest = dftest['MT18_XS'].values  # appends a list of fission cross sections to the XS_fission_train matrix
+
+	mt2xstest = dftest['MT2_XS'].values  # likewise for elastic scattering cross sections
+
+	fission_test.append(mt18xstest)
+	elastic_test.append(mt2xstest)
+
+
+y_test = np.array(keff_test)
+keff_mean = np.mean(keff_test)
+keff_std = np.std(keff_test)
+y_test = zscore(y_test)
+
+scaled_fission_test = scaler(fission_test)
+scaled_elastic_test = scaler(elastic_test)
+X_test = []
+
+for f_data, e_data in zip(scaled_fission_test, scaled_elastic_test):
+	X_test.append([f_data, e_data])
+
+X_test = np.array(X_test)
+X_test = X_test.reshape([X_test.shape[0], X_test.shape[2], X_test.shape[1]])
+print('Test data processed...')
+
