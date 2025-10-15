4ca2ed9d6a507c474e392c0d593fea492585e60c
Rohan-TG <rohan.tg05@gmail.com>
2025-10-15 12:41:01 +0100

    testing data

--- ml/multichannel-mlp/test_morris.py
+++ ml/multichannel-mlp/test_morris.py
@@ -15,174 +15,127 @@ training_fission_data_directory = '/home/rnt26/PycharmProjects/uncertaintyanalys
 training_elastic_data_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata/Pu239/elastic_scattering/g4'
 test_data_directory = '/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata/g4_fission_elastic_pu9'
 
-g4boundary = Groups.g4
+g4boundary = Groups.g4a
 g3boundary = Groups.g3
 
-all_fission_parquets = os.listdir(training_fission_data_directory)
-all_elastic_parquets = os.listdir(training_elastic_data_directory)
+all_fission_parquets = [os.path.join(training_fission_data_directory, f) for f in os.listdir(training_fission_data_directory)]
+all_elastic_parquets = [os.path.join(training_elastic_data_directory, f) for f in os.listdir(training_elastic_data_directory)]
 
 perturbation_set = np.arange(-0.500, 0.501, 0.001)
 
 training_fraction = 1.0
 n_training_samples = int(training_fraction * len(perturbation_set))
 
-training_files = all_fission_parquets + all_elastic_parquets
 # while len(training_files) < n_training_samples:
 # 	choice = random.choice(perturbation_set)
 # 	if choice not in training_files:
 # 		training_files.append(choice)
 
-# test_files = os.listdir(test_data_directory)
+test_files = os.listdir(test_data_directory)
 # # for file in all_parquets:
 # # 	if file not in training_files:
 # # 		test_files.append(file)
+
+
+keff_train = [] # k_eff labels
+
+XS_train = []
+fission_train = []
+elastic_train = []
+
+for file in tqdm.tqdm(all_fission_parquets, total=len(all_fission_parquets)):
+	dftrain = pd.read_parquet(f'{file}', engine='pyarrow') # Fetch data from parquet file
+
+	keff_train += [float(dftrain['keff'].values[0])] # append k_eff value from the file
+
+	dftrain = dftrain[dftrain.ERG >= g4boundary]
+	dftrain = dftrain[dftrain.ERG <= g3boundary]
+
+	mt18xs = np.log(dftrain['XS'].values) # appends a list of cross sections to the XS_train matrix
+
+	elastic_df = pd.read_parquet('/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata/Pu239/elastic_scattering/g4/Pu-239_g4_0.000_MT2.parquet',
+								 engine='pyarrow')
 #
+	mt2xs = np.log(elastic_df['XS'].values) # likewise for elastic scattering cross sections
 #
-# keff_train = [] # k_eff labels
-#
-# XS_train = []
-#
-# for file in tqdm.tqdm(training_files, total=len(training_files)):
-# 	dftrain = pd.read_parquet(f'{data_directory}/{file}', engine='pyarrow') # Fetch data from parquet file
-#
-# 	keff_train += [float(dftrain['keff'].values[0])] # append k_eff value from the file
-#
-# 	dftrain = dftrain[dftrain.ERG >= g4boundary]
-# 	dftrain = dftrain[dftrain.ERG <= g3boundary]
-#
-# 	mt18xs = np.log(dftrain['MT18_XS'].values) # appends a list of fission cross sections to the XS_fission_train matrix
-#
-# 	mt2xs = np.log(dftrain['MT2_XS'].values) # likewise for elastic scattering cross sections
-#
-# 	mt18xs = mt18xs.tolist()
-# 	mt2xs = mt2xs.tolist()
+	# mt18xs = mt18xs.tolist()
+	# mt2xs = mt2xs.tolist()
 #
 # 	xsobject = mt2xs + mt18xs
-# 	XS_train.append(xsobject)
-#
-# XS_train = np.array(XS_train)
-# y_train = np.array(keff_train)
-# y_train = zscore(y_train)
-#
-# keff_train_mean = np.mean(keff_train)
-# keff_train_std = np.std(keff_train)
-#
-# scaling_matrix_xtrain = XS_train.transpose()
-#
-# scaled_columns_xtrain = []
-# for column in tqdm.tqdm(scaling_matrix_xtrain[1:], total=len(scaling_matrix_xtrain[1:])):
-# 	scaled_column = zscore(column)
-# 	scaled_columns_xtrain.append(scaled_column)
-#
-# scaled_columns_xtrain = np.array(scaled_columns_xtrain)
-# Transposed_scaled_xtrain = scaled_columns_xtrain.transpose()
-#
-# X_train = []
-# for i in Transposed_scaled_xtrain:
-# 	row = i[~np.isnan(i)]
-# 	X_train.append(row)
-#
-# X_train = np.array(X_train)
-#
-#
-# print('Training data processed...')
-#
-#
-# ########################################## Test data preparation #######################################################
-# XS_test = []
-# keff_test = []
-# for testfile in tqdm.tqdm(test_files, total=len(test_files)):
-# 	dftest = pd.read_parquet(f'{data_directory}/{testfile}', engine='pyarrow')
-# 	keff_test += [float(dftest['keff'].values[0])]
-#
-# 	dftest = dftest[dftest.ERG >= g4boundary]
-# 	dftest = dftest[dftest.ERG <= g3boundary]
-#
-# 	mt18xstest = np.log(dftest['MT18_XS'].values)  # appends a list of fission cross sections to the XS_fission_train matrix
-#
-# 	mt2xstest = np.log(dftest['MT2_XS'].values)  # likewise for elastic scattering cross sections
-#
-# 	mt18xstest = mt18xstest.tolist()
-# 	mt2xstest = mt2xstest.tolist()
-#
-# 	xsobjecttest = mt2xstest + mt18xstest
-#
-# 	XS_test.append(xsobjecttest)
-#
-# XS_test = np.array(XS_test)
-# keff_mean = np.mean(keff_test)
-# keff_std = np.std(keff_test)
-# y_test = zscore(keff_test)
-#
-# scaling_matrix_xtest = XS_test.transpose()
-#
-# scaled_columns_xtest = []
-# for columntest in tqdm.tqdm(scaling_matrix_xtest[1:], total=len(scaling_matrix_xtest[1:])):
-# 	scaled_column_test = zscore(columntest)
-# 	scaled_columns_xtest.append(scaled_column_test)
-#
-# scaled_columns_xtest = np.array(scaled_columns_xtest)
-# Transposed_scaled_xtest = scaled_columns_xtest.transpose()
-#
-# X_test = []
-# for i in Transposed_scaled_xtest:
-# 	row = i[~np.isnan(i)]
-# 	X_test.append(row)
-#
-# X_test = np.array(X_test)
-#
-# callback = keras.callbacks.EarlyStopping(monitor='val_loss',
-# 										 # min_delta=0.005,
-# 										 patience=20,
-# 										 mode='min',
-# 										 start_from_epoch=5,
-# 										 restore_best_weights=True)
-#
-# model =keras.Sequential()
-# model.add(keras.layers.Dense(10, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
-# model.add(keras.layers.Dense(10, activation='relu'))
-# # model.add(keras.layers.Dense(100, activation='relu'))
-# # model.add(keras.layers.Dense(1000, activation='relu'))
-# # model.add(keras.layers.Dense(500, activation='relu'))
-# model.add(keras.layers.Dense(1, activation='linear'))
-# model.compile(loss='MeanSquaredError', optimizer='adam')
-#
-#
-# trainstart = time.time()
-# history = model.fit(X_train,
-# 					y_train,
-# 					epochs=100,
-# 					batch_size=4,
-# 					callbacks=callback,
-# 					validation_data=(X_test, y_test),
-# 					verbose=1)
-#
-#
-# train_end = time.time()
-# print(f'Training completed in {datetime.timedelta(seconds=(train_end - trainstart))}')
-# predictions = model.predict(X_test)
-# predictions = predictions.ravel()
-#
-#
-#
-# rescaled_predictions = []
-# predictions_list = predictions.tolist()
-#
-# for pred in predictions_list:
-# 	descaled_p = pred * keff_std + keff_mean
-# 	rescaled_predictions.append(float(descaled_p))
-#
-# errors = []
-# for predicted, true in zip(rescaled_predictions, keff_test):
-# 	errors.append((predicted - true) * 1e5)
-# 	print(f'SCONE: {true:0.5f} - ML: {predicted:0.5f}, Difference = {(predicted - true) * 1e5:0.0f} pcm')
-#
-#
-# sorted_errors = sorted(errors)
-# absolute_errors = [abs(x) for x in sorted_errors]
-# print(f'Average absolute error: {np.mean(absolute_errors)} +- {np.std(absolute_errors)}')
-#
-# print(f'Max -ve error: {sorted_errors[0]} pcm, Max +ve error: {sorted_errors[-1]} pcm')
-#
-#
-# print(f"Smallest absolute error: {min(absolute_errors)} pcm")
+	fission_train.append(mt18xs)
+	elastic_train.append(mt2xs)
+
+for file in tqdm.tqdm(all_elastic_parquets, total=len(all_elastic_parquets)):
+	dftrain = pd.read_parquet(f'{file}', engine='pyarrow')  # Fetch data from parquet file
+
+	keff_train += [float(dftrain['keff'].values[0])]  # append k_eff value from the file
+
+	dftrain = dftrain[dftrain.ERG >= g4boundary]
+	dftrain = dftrain[dftrain.ERG <= g3boundary]
+
+	mt2xs = np.log(dftrain['XS'].values)  # appends a list of cross sections to the XS_train matrix
+
+	fission_df = pd.read_parquet(
+		'/home/rnt26/PycharmProjects/uncertaintyanalysis/ml/mldata/Pu239/fission/g4/Pu-239_g4_0.000_MT18.parquet',
+		engine='pyarrow')
+	#
+	mt18xs = np.log(fission_df['XS'].values)  # likewise for elastic scattering cross sections
+	#
+	# mt18xs = mt18xs.tolist()
+	# mt2xs = mt2xs.tolist()
+	#
+	# 	xsobject = mt2xs + mt18xs
+	fission_train.append(mt18xs)
+	elastic_train.append(mt2xs)
+
+
+y_train = np.array(keff_train)
+y_train = zscore(y_train)
+
+keff_train_mean = np.mean(keff_train)
+keff_train_std = np.std(keff_train)
+
+
+
+
+
+
+
+
+
+
+
+def scaler(channel_matrix):
+
+	transposed_matrix = np.transpose(np.array(channel_matrix))
+	scaled_rows = []
+	for row in tqdm.tqdm(transposed_matrix, total=len(transposed_matrix)):
+		scaled_row = zscore(row)
+		scaled_rows.append(scaled_row)
+
+	scaled_rows = np.array(scaled_rows)
+	transposed_scaled_matrix = np.transpose(scaled_rows)
+
+	final_matrix = []
+	for i in transposed_scaled_matrix:
+		row = i[~np.isnan(i)]
+		final_matrix.append(row)
+
+	final_matrix = np.array(final_matrix)
+	return final_matrix
+
+
+
+scaled_fission_train = scaler(fission_train)
+scaled_elastic_train = scaler(elastic_train)
+
+
+X_train = []
+for f_data, e_data in zip(scaled_fission_train, scaled_elastic_train):
+	X_train.append([f_data, e_data])
+
+X_train = np.array(X_train)
+X_train = X_train.reshape([X_train.shape[0], X_train.shape[2], X_train.shape[1]])
+
+
+print('Training data processed...')
