95d3d991e537f13e22b5a92ecc1040f4aa299b9d
Rohan-TG <rohan.tg05@gmail.com>
2025-10-20 14:24:57 +0100

    removed plt.show

--- utilities/flux_data_maker.py
+++ utilities/flux_data_maker.py
@@ -62,7 +62,7 @@ def extract_flux(output_file):
 		plt.ylabel('Flux')
 		plt.title(f'Flux for {coefficient * 100}% perturbation')
 		plt.savefig(f'{saveplot_directory}/{output_file}.png', dpi=300)
-		plt.show()
+		# plt.show()
 
 with ProcessPoolExecutor(max_workers=processes) as executor:
 	futures = [executor.submit(extract_flux, out_file) for out_file in output_files]
