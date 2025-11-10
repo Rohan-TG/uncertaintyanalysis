a41304911d6818e1c013fdd0865df5d3c24eeb20
Rohan-TG <rohan.tg05@gmail.com>
2025-11-10 16:36:29 +0000

    pathing

--- dataprocessing/random-files/Pu239/fission/sandy-random-pu9-mt18.py
+++ dataprocessing/random-files/Pu239/fission/sandy-random-pu9-mt18.py
@@ -1,5 +1,11 @@
 import sandy
-
+import os
+import sys
+computer = os.uname().nodename
+if computer == 'fermiac':
+	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/') # change depending on machine
+elif computer == 'oppie':
+	sys.path.append('/home/rnt26/uncertaintyanalysis/')
 from groupEnergies import Pu239
 
 lib_name = "ENDFB_80"
