a8cda26d33463f1162becdfaf38162024b5ebd5a
Rohan-TG <rohan.tg05@gmail.com>
2025-11-22 16:46:51 +0000

    found source

--- ml/physor/mlp_most_groups.py
+++ ml/physor/mlp_most_groups.py
@@ -58,13 +58,8 @@ for file in tqdm.tqdm(training_files, total=len(training_files)):
 	dftrain = dftrain[dftrain.ERG <= g0boundary]
 
 	mt18xs = dftrain['XS'].values  # appends a list of fission cross sections to the XS_fission_train matrix
+	mt18xs = mt18xs[1:391]
 
-	# mt2xs = np.log(dftrain['MT2_XS'].values)  # likewise for elastic scattering cross sections
-
-	# mt18xs = mt18xs.tolist()
-	# mt2xs = mt2xs.tolist()
-
-	# xsobject = mt2xs + mt18xs
 	XS_train.append(mt18xs)
 
 XS_train = np.array(XS_train)
@@ -73,7 +68,7 @@ y_train = zscore(keff_train)
 scaling_matrix_xtrain = XS_train.transpose()
 
 scaled_columns_xtrain = []
-for column in tqdm.tqdm(scaling_matrix_xtrain[1:390], total=len(scaling_matrix_xtrain[1:390])):
+for column in tqdm.tqdm(scaling_matrix_xtrain[1:], total=len(scaling_matrix_xtrain[1:])):
 	scaled_column = zscore(column)
 	scaled_columns_xtrain.append(scaled_column)
 
@@ -87,7 +82,7 @@ for file in tqdm.tqdm(test_files, total=len(test_files)):
 	# group = file.split('_')[1][1]
 	dftest = pd.read_parquet(f'{file}', engine='pyarrow')
 	keff_test += [float(dftest['keff'].values[0])]
-	XS_test.append(dftest['XS'].values)
+	XS_test.append(dftest['XS'].values[1:391])
 
 XS_test = np.array(XS_test)
 keff_mean = np.mean(keff_test)
@@ -97,7 +92,7 @@ y_test = zscore(keff_test)
 scaling_matrix_xtest = XS_test.transpose()
 
 scaled_columns_xtest = []
-for column in tqdm.tqdm(scaling_matrix_xtest[1:390], total=len(scaling_matrix_xtest[1:390])):
+for column in tqdm.tqdm(scaling_matrix_xtest[1:], total=len(scaling_matrix_xtest[1:])):
 	scaled_column = zscore(column)
 	scaled_columns_xtest.append(scaled_column)
 
