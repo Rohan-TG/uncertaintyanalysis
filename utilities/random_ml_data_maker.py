66ee9af0e35caff5f9f92e60d7c72cefd2902061
Rohan-TG <rohan.tg05@gmail.com>
2025-12-04 16:49:04 +0000

    wip

--- utilities/random_ml_data_maker.py
+++ utilities/random_ml_data_maker.py
@@ -9,9 +9,36 @@ elif computer == 'oppie':
 
 import pandas as pd
 import ENDF6
+import tqdm
 
 isotope = input("Enter Element-nucleon_number: ")
 MT = int(input("Enter MT number: "))
 outputs_directory = input("Enter SCONE output directory: ")
 pendf_dir = input("Enter PENDF directory: ")
 parquet_directory = os.getcwd()
+
+
+output_files = os.listdir(outputs_directory)
+
+keff_list = []
+keff_error_list = []
+
+print("Reading output files...")
+for outputfile in tqdm.tqdm(output_files, total=len(output_files)):
+	read_object = open(f'{outputs_directory}/{outputfile}', 'r')
+
+	file_index = int(outputfile.split('.m')[0].split('-')[1])
+
+
+
+	lines = read_object.readlines()
+	keffline = lines[12]
+
+	keff_value_string = keffline[15:26]
+	keff_value_float = float(keff_value_string.replace('E', 'e'))
+	keff_list.append(keff_value_float)
+
+	keff_error = keffline[27:38]
+	keff_error_float = float(keff_error.replace('E', 'e'))
+	keff_error_list.append(keff_error_float)
+
