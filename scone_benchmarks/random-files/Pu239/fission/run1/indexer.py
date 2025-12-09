562ec24b6d645a7a1dd68dd994a851c84342ee53
Rohan-TG <rohan.tg05@gmail.com>
2025-12-09 16:01:41 +0000

    temporary

--- scone_benchmarks/random-files/Pu239/fission/run1/indexer.py
+++ scone_benchmarks/random-files/Pu239/fission/run1/indexer.py
@@ -3,12 +3,17 @@ import numpy as np
 import subprocess
 import tqdm
 
-# pendf_dir = '/home/rnt26/uncertaintyanalysis/dataprocessing/random-files/Pu239/fission/run3/pendf'
-ace_dir = '/home/rnt26/uncertaintyanalysis/dataprocessing/random-files/Pu239/fission/run2000_3999/ace'
+pendf_dir = '/home/rnt26/uncertaintyanalysis/dataprocessing/random-files/Pu239/fission/run2000_3999/pendf'
+# ace_dir = '/home/rnt26/uncertaintyanalysis/dataprocessing/random-files/Pu239/fission/run2000_3999/ace'
+
+# mode = input('ace/pendf: ')
+
+# if mode == 'ace':
 
 
-# pendfs = os.listdir(pendf_dir)
-aces = os.listdir(ace_dir)
+
+pendfs = os.listdir(pendf_dir)
+# 	aces = os.listdir(ace_dir)
 
 
 original_indices = list(range(0,2000))
@@ -16,16 +21,16 @@ new_indices = list(range(2000, 4000))
 
 for old_i, new_i in tqdm.tqdm(zip(original_indices, new_indices), total=len(original_indices)):
 
-	new_ace_filename = f'94239_{new_i}.03c'
-	old_ace_filename = f'94239_{old_i}.03c'
+	# new_ace_filename = f'94239_{new_i}.03c'
+	# old_ace_filename = f'94239_{old_i}.03c'
 
 
-	# new_pendf_filename = f'94239_{new_i}.pendf'
-	# old_pendf_filename = f'94239_{old_i}.pendf'
+	new_pendf_filename = f'94239_{new_i}.pendf'
+	old_pendf_filename = f'94239_{old_i}.pendf'
 
 	print(f'{old_i} -> {new_i} \n')
 
-	subprocess.run(f'mv {ace_dir}/{old_ace_filename} {ace_dir}/{new_ace_filename}', shell=True)
+	# subprocess.run(f'mv {ace_dir}/{old_ace_filename} {ace_dir}/{new_ace_filename}', shell=True)
 
-	# subprocess.run(f'mv {pendf_dir}/{old_pendf_filename} {pendf_dir}/{new_ace_filename}', shell=True)
+	subprocess.run(f'mv {pendf_dir}/{old_pendf_filename} {pendf_dir}/{new_pendf_filename}', shell=True)
 
