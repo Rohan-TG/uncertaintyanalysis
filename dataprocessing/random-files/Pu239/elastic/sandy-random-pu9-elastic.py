6b677814b97b2c4cec38720d3a7415c6a8f6c5c1
Rohan-TG <rohan.tg05@gmail.com>
2025-12-11 15:43:31 +0000

    run on oppie

---
+++ dataprocessing/random-files/Pu239/elastic/sandy-random-pu9-elastic.py
@@ -0,0 +1,64 @@
+import sandy
+import os
+import sys
+computer = os.uname().nodename
+if computer == 'fermiac':
+	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/') # change depending on machine
+elif computer == 'oppie':
+	sys.path.append('/home/rnt26/uncertaintyanalysis/')
+from groupEnergies import Pu239, Reactions
+
+lib_name = "ENDFB_80"
+nucl = Pu239.ZA * 10
+filename = f"{nucl}.{lib_name}"
+
+endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
+endf6.to_file(filename)
+pendf = endf6.get_pendf(err=0.0001)
+
+num_samples = 2000  # number of samples
+processes = 15
+
+MT = Reactions.elastic
+
+
+samples = endf6.get_perturbations(
+	num_samples,
+	njoy_kws=dict(
+		err=0.0001,
+		errorr33_kws=dict(mt=[MT]),
+		chi=False,
+		mubar=False,
+		xs=True,
+		nubar=False,
+		verbose=True,
+	),
+)
+
+
+outs = endf6.apply_perturbations( # generates the PENDFs only
+	samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),  # low error
+	# to_ace=True,   # produce ACE files
+	to_file=True,
+	# ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+	verbose=True,
+)
+
+# this generates samples for cross sections and nubar
+
+
+
+
+ace_outs = endf6.apply_perturbations(
+	samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),
+	to_ace = True,
+	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+	to_file=True,
+	verbose=True,
+)
+
+
