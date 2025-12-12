b623a7b1b51029a05e23644764129b161721dae0
Rohan-TG <rohan.tg05@gmail.com>
2025-12-12 18:17:53 +0000

    wip

---
+++ dataprocessing/random-files/Pu239/sensitive_channels/run_sandy_sensitive_channels_pu239.py
@@ -0,0 +1,10 @@
+import sandy
+import sys
+import os
+
+computer = os.uname().nodename
+if computer == 'fermiac':
+	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/')
+elif computer == 'oppie':
+	sys.path.append('/home/rnt26/uncertaintyanalysis/')
+
