4aff3955e14ad49df55003f5656a48a45071089f
Rohan-TG <rohan.tg05@gmail.com>
2025-12-12 19:50:20 +0000

    removed errorr33 kwords

--- dataprocessing/random-files/Pu239/sensitive_channels/run_sandy_sensitive_channels_pu239.py
+++ dataprocessing/random-files/Pu239/sensitive_channels/run_sandy_sensitive_channels_pu239.py
@@ -23,17 +23,17 @@ endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
 num_samples = 1  # number of samples
 processes = 1
 
-fission_channel = Reactions.fission
-elastic_channel = Reactions.elastic
-inelastic_channel = Reactions.inelastic
-capture_channel = Reactions.capture
+# fission_channel = Reactions.fission
+# elastic_channel = Reactions.elastic
+# inelastic_channel = Reactions.inelastic
+# capture_channel = Reactions.capture
 
 
 samples = endf6.get_perturbations(
 	num_samples,
 	njoy_kws=dict(
 		err=0.0001,
-		errorr33_kws=dict(mt=[fission_channel]),
+		# errorr33_kws=dict(mt=[fission_channel]),
 		chi=False,
 		mubar=False,
 		xs=True,
@@ -68,3 +68,47 @@ ace_outs = endf6.apply_perturbations(
 	to_file=True,
 	verbose=True,
 )
+
+
+
+#
+#
+# fission_perturbed_endf6_file = sandy.Endf6.from_file('/home/rnt26/PycharmProjects/uncertaintyanalysis/dataprocessing/random-files/Pu239/sensitive_channels/fission_only_test/94239_0.endf6')
+#
+#
+# samples_elastic = fission_perturbed_endf6_file.get_perturbations(
+# 	num_samples,
+# 	njoy_kws=dict(
+# 		err=0.0001,
+# 		errorr33_kws=dict(mt=[elastic_channel]),
+# 		chi=False,
+# 		mubar=False,
+# 		xs=True,
+# 		nubar=False,
+# 		verbose=True,
+# 	),
+# )
+
+# gone up to here
+#
+#
+# outs_elastic = fission_perturbed_endf6_file.apply_perturbations( # generates the PENDFs only
+# 	samples_elastic,
+# 	processes=processes,
+# 	njoy_kws=dict(err=0.0001),  # low error
+# 	# to_ace=True,   # produce ACE files
+# 	to_file=True,
+# 	# ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+# 	verbose=True,
+# )
+#
+#
+# ace_outs_elastic = fission_perturbed_endf6_file.apply_perturbations(
+# 	samples_elastic,
+# 	processes=processes,
+# 	njoy_kws=dict(err=0.0001),
+# 	to_ace = True,
+# 	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+# 	to_file=True,
+# 	verbose=True,
+# )
