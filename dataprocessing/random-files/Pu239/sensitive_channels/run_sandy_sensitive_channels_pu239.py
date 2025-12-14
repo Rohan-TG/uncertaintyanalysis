3a09989a959f74da375003cd48b9e38f766b87cd
Rohan-TG <rohan.tg05@gmail.com>
2025-12-14 14:50:17 +0000

    fully random files

--- dataprocessing/random-files/Pu239/sensitive_channels/run_sandy_sensitive_channels_pu239.py
+++ dataprocessing/random-files/Pu239/sensitive_channels/run_sandy_sensitive_channels_pu239.py
@@ -8,7 +8,7 @@ if computer == 'fermiac':
 	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/')
 elif computer == 'oppie':
 	sys.path.append('/home/rnt26/uncertaintyanalysis/')
-from groupEnergies import Pu239, Reactions
+from groupEnergies import Pu239
 
 
 lib_name = "ENDFB_80"
@@ -20,20 +20,15 @@ endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
 # pendf = endf6.get_pendf(err=0.0001)
 
 
-num_samples = 1  # number of samples
-processes = 1
+num_samples = 1000  # number of samples
+processes = 5
 
-# fission_channel = Reactions.fission
-# elastic_channel = Reactions.elastic
-# inelastic_channel = Reactions.inelastic
-# capture_channel = Reactions.capture
 
 
 samples = endf6.get_perturbations(
 	num_samples,
 	njoy_kws=dict(
 		err=0.0001,
-		# errorr33_kws=dict(mt=[fission_channel]),
 		chi=False,
 		mubar=False,
 		xs=True,
@@ -41,12 +36,6 @@ samples = endf6.get_perturbations(
 		verbose=True,
 	),
 )
-
-# samples_updated = endf6.get_perturbations(
-# 	num_samples,
-#
-# )
-
 
 outs = endf6.apply_perturbations( # generates the PENDFs only
 	samples,
@@ -70,45 +59,3 @@ ace_outs = endf6.apply_perturbations(
 )
 
 
-
-#
-#
-# fission_perturbed_endf6_file = sandy.Endf6.from_file('/home/rnt26/PycharmProjects/uncertaintyanalysis/dataprocessing/random-files/Pu239/sensitive_channels/fission_only_test/94239_0.endf6')
-#
-#
-# samples_elastic = fission_perturbed_endf6_file.get_perturbations(
-# 	num_samples,
-# 	njoy_kws=dict(
-# 		err=0.0001,
-# 		errorr33_kws=dict(mt=[elastic_channel]),
-# 		chi=False,
-# 		mubar=False,
-# 		xs=True,
-# 		nubar=False,
-# 		verbose=True,
-# 	),
-# )
-
-# gone up to here
-#
-#
-# outs_elastic = fission_perturbed_endf6_file.apply_perturbations( # generates the PENDFs only
-# 	samples_elastic,
-# 	processes=processes,
-# 	njoy_kws=dict(err=0.0001),  # low error
-# 	# to_ace=True,   # produce ACE files
-# 	to_file=True,
-# 	# ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
-# 	verbose=True,
-# )
-#
-#
-# ace_outs_elastic = fission_perturbed_endf6_file.apply_perturbations(
-# 	samples_elastic,
-# 	processes=processes,
-# 	njoy_kws=dict(err=0.0001),
-# 	to_ace = True,
-# 	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
-# 	to_file=True,
-# 	verbose=True,
-# )
