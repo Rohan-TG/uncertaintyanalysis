96ef1028e558254e568cc664a45b7513419f4a95
Rohan-TG <rohan.tg05@gmail.com>
2025-12-15 15:35:02 +0000

    ready to run scone

---
+++ scone_benchmarks/random-files/Pu239/all-channels/run-scone-all-channels.py
@@ -0,0 +1,79 @@
+import numpy as np
+import tqdm
+import time
+import subprocess
+import datetime
+import os
+import sys
+
+sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis')
+from groupEnergies import Pu239, Reactions
+
+start_time = time.time()
+ZA = Pu239.ZA # ZA for Pu-240
+
+ACE_file_directory = input("Enter ACE file directory: ")
+scone_executable_path = '/home/rnt26/scone/SCONE/Build/scone.out'
+
+
+num_cores = int(input('Num. cores: ')) # number of cores to use for this specific instance of scone
+
+
+default_xsfile = 'endfb-viii0.xsfile'
+current_dir = os.getcwd()
+target_dir = 'scone_benchmarks'
+idxt = current_dir.find(target_dir)
+default_Jezebel = 'Jezebel'
+
+target_path = current_dir[:(idxt + len(target_dir))]
+libfilename = f'lib{ZA}.xsfile'
+mt = Reactions.fission
+
+subprocess.run(f"cp {target_path}/{default_xsfile} {libfilename}", shell=True)
+subprocess.run(f"cp {target_path}/{default_Jezebel} Jezebel", shell=True)
+
+
+search_files = os.listdir()
+for file in search_files:
+	if '.xsfile' in file:
+		libfile = file
+		break
+
+xsfile_fullpath = os.path.abspath(libfile)
+os.environ["SCONE_ACE"] = xsfile_fullpath
+
+if 'outputfiles' not in search_files:
+	subprocess.run('mkdir outputfiles', shell=True)
+
+
+ACE_files = os.listdir(ACE_file_directory)
+
+for ace_file in tqdm.tqdm(ACE_files):
+	file_index = int(ace_file.split('.03c')[0].split('_')[1])
+
+	ACE_filename = f'{ACE_file_directory}/94239_{file_index}.03c' # ACE file for this iteration
+
+	with open(libfile, 'r') as file: # the ACE address file (.xsfile)
+		lines = file.readlines()
+
+	try:
+		for i, ACE_address in enumerate(lines):
+			if str(ZA) in ACE_address:
+				Nuclide_ACE_address = i
+				break
+	except:
+		print('Cannot find ACE address match. Terminating')
+		break
+
+	lines[Nuclide_ACE_address] = f'{ZA}.00c; 1; {ACE_filename};\n'  # Rewrite line
+
+	with open(libfile, 'w') as file:  # write to new .xsfile
+		file.writelines(lines)
+
+	subprocess.run(f'{scone_executable_path} --omp {num_cores} Jezebel', shell=True)  # run scone
+	subprocess.run(f'mv output.m outputfiles/output-{int(file_index)}.m',
+				   shell=True)  # move output file to output directory for later analysis
+
+end_time = time.time()
+
+print(f"Time elapsed: {datetime.timedelta(seconds=end_time-start_time)}")
