96c684472dd31b62b68cd02c79fc5e98c2d2de7e
Rohan-TG <rohan.tg05@gmail.com>
2025-12-17 15:16:45 +0000

    run on fermiac

---
+++ dataprocessing/random-files/Pu240/all-channels/run-sandy-random-files-pu240.py
@@ -0,0 +1,62 @@
+import sandy
+import sys
+import os
+
+
+computer = os.uname().nodename
+if computer == 'fermiac':
+	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/')
+elif computer == 'oppie':
+	sys.path.append('/home/rnt26/uncertaintyanalysis/')
+from groupEnergies import Pu240
+
+
+lib_name = "ENDFB_80"
+nucl = Pu240.ZA * 10
+filename = f"{nucl}.{lib_name}"
+
+endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
+# endf6.to_file(filename)
+# pendf = endf6.get_pendf(err=0.0001)
+
+
+num_samples = 2000  # number of samples
+processes = 5
+
+
+
+samples = endf6.get_perturbations(
+	num_samples,
+	njoy_kws=dict(
+		err=0.0001,
+		chi=False,
+		mubar=False,
+		xs=True,
+		nubar=False,
+		verbose=True,
+	),
+)
+
+outs = endf6.apply_perturbations( # generates the PENDFs only
+	samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),  # low error
+	# to_ace=True,   # produce ACE files
+	to_file=True,
+	# ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+	verbose=True,
+)
+
+
+ace_outs = endf6.apply_perturbations(
+	samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),
+	to_ace = True,
+	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+	to_file=True,
+	verbose=True,
+)
+
+
+
