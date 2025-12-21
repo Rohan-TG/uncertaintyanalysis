0bf06d418e7ce1080f4055b39c3ba512a05ffe70
Rohan-TG <rohan.tg05@gmail.com>
2025-12-21 17:07:07 +0000

    wip

--- scone_benchmarks/random-files/all_nuclides/run_scone_all_observables.py
+++ scone_benchmarks/random-files/all_nuclides/run_scone_all_observables.py
@@ -8,6 +8,47 @@ elif computer == 'oppie':
 from groupEnergies import Pu239, Reactions
 import time
 import tqdm
+import subprocess
 
 starttime = time.time()
+
+num_cores = int(input('Num. cores: '))
+
+default_xsfile = 'endfb-viii0.xsfile'
+current_dir = os.getcwd()
+target_dir = 'scone_benchmarks'
+idxt = current_dir.find(target_dir)
+default_Jezebel = 'Jezebel'
+
+target_path = current_dir[:(idxt + len(target_dir))]
+libfilename = f'allobservables.xsfile'
+
+subprocess.run(f"cp {target_path}/{default_xsfile} {libfilename}", shell=True)
+subprocess.run(f"cp {target_path}/{default_Jezebel} Jezebel", shell=True)
+
+
+search_files = os.listdir()
+for file in search_files:
+	if '.xsfile' in file:
+		libfile = file
+		break
+
+xsfile_fullpath = os.path.abspath(libfile)
+os.environ["SCONE_ACE"] = xsfile_fullpath
+
+if 'outputfiles' not in search_files:
+	subprocess.run('mkdir outputfiles', shell=True)
+
+
+
+Pu239_ACE_file_directory = input('Enter Pu239 ACE file directory: ')
+Pu239_ACE_files = os.listdir(Pu239_ACE_file_directory)
+
+Pu240_ACE_file_directory = input('Enter Pu240 ACE file directory: ')
+Pu240_ACE_files = os.listdir(Pu240_ACE_file_directory)
+
+Pu241_ACE_file_directory = input('Enter Pu241 ACE file directory: ')
+Pu241_ACE_files = os.listdir(Pu241_ACE_file_directory)
+
+
 
