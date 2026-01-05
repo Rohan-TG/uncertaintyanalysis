9b9ebc60b719f1cd32cdee8a2f1c4e217eaea8af
Rohan-TG <rohan.tg05@gmail.com>
2026-01-05 19:37:24 +0000

    increased number of nodes

--- ml/random/mlp/pu_only_all_channels.py
+++ ml/random/mlp/pu_only_all_channels.py
@@ -228,10 +228,10 @@ callback = keras.callbacks.EarlyStopping(monitor='val_loss',
 
 
 model =keras.Sequential()
-model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
-# model.add(keras.layers.Dense(175, activation='relu'))
-model.add(keras.layers.Dense(100, activation='relu'))
-model.add(keras.layers.Dense(70, activation='relu'))
+model.add(keras.layers.Dense(400, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+model.add(keras.layers.Dense(375, activation='relu'))
+model.add(keras.layers.Dense(200, activation='relu'))
+model.add(keras.layers.Dense(170, activation='relu'))
 model.add(keras.layers.Dense(40, activation='relu'))
 model.add(keras.layers.Dense(1, activation='linear'))
 model.compile(loss='MeanSquaredError', optimizer='adam')
