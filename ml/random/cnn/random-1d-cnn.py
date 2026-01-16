f3d8e599c078944f5cd4bfc03c6bf20a6577e4c9
Rohan-TG <rohan.tg05@gmail.com>
2026-01-16 15:58:39 +0000

    increased num filters

--- ml/random/cnn/random-1d-cnn.py
+++ ml/random/cnn/random-1d-cnn.py
@@ -203,7 +203,7 @@ callback = keras.callbacks.EarlyStopping(monitor='val_loss',
 model = keras.Sequential()
 model.add(keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2])))
 model.add(keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu',))
-model.add(keras.layers.Conv1D(filters=16, kernel_size=3, padding='same', activation='relu',))
+model.add(keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu',))
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(500, activation='relu'))
 model.add(keras.layers.Dense(300, activation='relu'))
