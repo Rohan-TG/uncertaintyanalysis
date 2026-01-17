9143338f90e598d953083e4822134a6abd1e3618
Rohan-TG <rohan.tg05@gmail.com>
2026-01-17 15:52:50 +0000

    testing on oppie

---
+++ utilities/move_by_index.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import tqdm
+
+
+ZA = input('Enter ZA (e.g. 94239): ')
+home_directory = input("Enter home directory (here for cwd): ")
+if home_directory == "cwd":
+	home_directory = os.getcwd()
+
+mode = input("Enter mode ace/pendf: ")
+if mode == "ace":
+	mode = '03c'
+
+move_index_lower = int(input('Enter lower index for move: '))
+move_index_upper = int(input('Enter upper index for move: '))
+
+target_directory = input('Enter target directory: ')
+files = os.listdir(home_directory)
+
+for i in tqdm.tqdm(range(move_index_lower, move_index_upper + 1)):
+	subprocess.run(f'mv {home_directory}/{ZA}_{i}.{mode} {target_directory}', shell=True)
