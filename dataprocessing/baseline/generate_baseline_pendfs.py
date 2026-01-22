cb9baa9fdb0c3818ebd347683c3867a185cc63d0
Rohan-TG <rohan.tg05@gmail.com>
2026-01-22 21:12:43 +0000

    pu241

--- dataprocessing/baseline/generate_baseline_pendfs.py
+++ dataprocessing/baseline/generate_baseline_pendfs.py
@@ -12,11 +12,11 @@ from groupEnergies import Pu239, Pu240, Pu241
 
 
 lib_name = "ENDFB_80"
-nucl = Pu240.ZA * 10
+nucl = Pu241.ZA * 10
 filename = f"{nucl}.{lib_name}"
 
 endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
 # endf6.to_file(filename)
 pendf = endf6.get_pendf(err=0.0001)
 
-pendf.to_file('Pu-240_ENDFBVIII.pendf')
+pendf.to_file('Pu-241_ENDFBVIII.pendf')
