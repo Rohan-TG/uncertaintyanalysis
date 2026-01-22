9e864fd5111e800aa69515a11f9f2e62a76c012f
Rohan-TG <rohan.tg05@gmail.com>
2026-01-22 21:11:55 +0000

    pu240

--- dataprocessing/baseline/generate_baseline_pendfs.py
+++ dataprocessing/baseline/generate_baseline_pendfs.py
@@ -8,15 +8,15 @@ if computer == 'fermiac':
 	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/')
 elif computer == 'oppie':
 	sys.path.append('/home/rnt26/uncertaintyanalysis/')
-from groupEnergies import Pu239
+from groupEnergies import Pu239, Pu240, Pu241
 
 
 lib_name = "ENDFB_80"
-nucl = Pu239.ZA * 10
+nucl = Pu240.ZA * 10
 filename = f"{nucl}.{lib_name}"
 
 endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
 # endf6.to_file(filename)
 pendf = endf6.get_pendf(err=0.0001)
 
-pendf.to_file('Pu-239_ENDFBVIII.pendf')
+pendf.to_file('Pu-240_ENDFBVIII.pendf')
