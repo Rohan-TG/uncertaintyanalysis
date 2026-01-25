c6963165fe14bea83ea133b73bc23b1cbeb9922a
Rohan-TG <rohan.tg05@gmail.com>
2026-01-25 16:11:23 +0000

    used wrong data processing method

--- ml/random/transformer/transformer_random.py
+++ ml/random/transformer/transformer_random.py
@@ -24,6 +24,9 @@ from scipy.stats import zscore
 import tqdm
 import time
 
+
+
+
 data_directory = input('Data directory: ')
 data_processes = int(input('Num. data processors: '))
 
@@ -31,128 +34,173 @@ all_parquets = os.listdir(data_directory)
 
 training_fraction = float(input('Enter training data fraction: '))
 lower_energy_bound = float(input('Enter lower energy bound in eV: '))
+
 n_training_samples = int(training_fraction * len(all_parquets))
 
 training_files = []
 while len(training_files) < n_training_samples:
-    choice = random.choice(all_parquets)
-    if choice not in training_files:
-        training_files.append(choice)
+	choice = random.choice(all_parquets)
+	if choice not in training_files:
+		training_files.append(choice)
 
 test_files = []
 for file in all_parquets:
-    if file not in training_files:
-        test_files.append(file)
+	if file not in training_files:
+		test_files.append(file)
 
 print('Fetching training data...')
 
+
+
+
+
 def fetch_data(datafile):
 
-    temp_df = pd.read_parquet(f'{data_directory}/{datafile}', engine='pyarrow')
-    temp_df = temp_df[temp_df['ERG'] >= lower_energy_bound]
+	temp_df = pd.read_parquet(f'{data_directory}/{datafile}', engine='pyarrow')
+	temp_df = temp_df[temp_df['ERG'] >= lower_energy_bound]
 
-    keff_value = float(temp_df['keff'].values[0])
+	keff_value = float(temp_df['keff'].values[0])
 
-    pu9_mt18xs = temp_df['94239_MT18_XS'].values.tolist()
-    pu0_mt18xs = temp_df['94240_MT18_XS'].values.tolist()
-    pu1_mt18xs = temp_df['94241_MT18_XS'].values.tolist()
+	pu9_mt18xs = temp_df['94239_MT18_XS'].values.tolist()
+	pu0_mt18xs = temp_df['94240_MT18_XS'].values.tolist()
+	pu1_mt18xs = temp_df['94241_MT18_XS'].values.tolist()
 
-    pu9_mt2xs = temp_df['94239_MT2_XS'].values.tolist()
-    pu0_mt2xs = temp_df['94240_MT2_XS'].values.tolist()
-    pu1_mt2xs = temp_df['94241_MT2_XS'].values.tolist()
+	pu9_mt2xs = temp_df['94239_MT2_XS'].values.tolist()
+	pu0_mt2xs = temp_df['94240_MT2_XS'].values.tolist()
+	pu1_mt2xs = temp_df['94241_MT2_XS'].values.tolist()
 
-    pu9_mt4xs = temp_df['94239_MT4_XS'].values.tolist()
-    pu0_mt4xs = temp_df['94240_MT4_XS'].values.tolist()
-    pu1_mt4xs = temp_df['94241_MT4_XS'].values.tolist()
+	pu9_mt4xs = temp_df['94239_MT4_XS'].values.tolist()
+	pu0_mt4xs = temp_df['94240_MT4_XS'].values.tolist()
+	pu1_mt4xs = temp_df['94241_MT4_XS'].values.tolist()
 
-    pu9_mt16xs = temp_df['94239_MT16_XS'].values.tolist()
-    pu0_mt16xs = temp_df['94240_MT16_XS'].values.tolist()
-    pu1_mt16xs = temp_df['94241_MT16_XS'].values.tolist()
+	pu9_mt16xs = temp_df['94239_MT16_XS'].values.tolist()
+	pu0_mt16xs = temp_df['94240_MT16_XS'].values.tolist()
+	pu1_mt16xs = temp_df['94241_MT16_XS'].values.tolist()
 
-    pu9_mt102xs = temp_df['94239_MT102_XS'].values.tolist()
-    pu0_mt102xs = temp_df['94240_MT102_XS'].values.tolist()
-    pu1_mt102xs = temp_df['94241_MT102_XS'].values.tolist()
+	pu9_mt102xs = temp_df['94239_MT102_XS'].values.tolist()
+	pu0_mt102xs = temp_df['94240_MT102_XS'].values.tolist()
+	pu1_mt102xs = temp_df['94241_MT102_XS'].values.tolist()
 
-    xsobject = pu9_mt2xs + pu9_mt4xs + pu9_mt16xs + pu9_mt18xs + pu9_mt18xs + pu9_mt102xs + pu0_mt2xs + pu0_mt4xs + pu0_mt16xs + pu0_mt18xs + pu0_mt102xs + pu1_mt2xs + pu1_mt2xs + pu1_mt4xs + pu1_mt16xs + pu1_mt18xs + pu1_mt102xs
+	# xsobject = pu9_mt2xs + pu9_mt4xs + pu9_mt16xs + pu9_mt18xs + pu9_mt18xs + pu9_mt102xs + pu0_mt2xs + pu0_mt4xs + pu0_mt16xs + pu0_mt18xs + pu0_mt102xs + pu1_mt2xs + pu1_mt2xs + pu1_mt4xs + pu1_mt16xs + pu1_mt18xs + pu1_mt102xs
+	#
+	# XS_obj = xsobject
 
-    XS_obj = xsobject
+	XS_obj = [pu9_mt2xs, pu9_mt4xs, pu9_mt16xs, pu9_mt18xs, pu9_mt102xs,
+				pu0_mt2xs, pu0_mt4xs, pu0_mt16xs, pu0_mt18xs, pu0_mt102xs,
+				pu1_mt2xs, pu1_mt4xs, pu1_mt16xs, pu1_mt18xs, pu1_mt102xs,]
 
-    return(XS_obj, keff_value)
+	return(XS_obj, keff_value)
+
+
 
 keff_train = [] # k_eff labels
 XS_train = []
 
 with ProcessPoolExecutor(max_workers=data_processes) as executor:
-    futures = [executor.submit(fetch_data, train_file) for train_file in training_files]
+	futures = [executor.submit(fetch_data, train_file) for train_file in training_files]
 
-    for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
-        xs_values, keff_value = future.result()
-        XS_train.append(xs_values)
-        keff_train.append(keff_value)
+	for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+		xs_values, keff_value = future.result()
+		XS_train.append(xs_values)
+		keff_train.append(keff_value)
 
-XS_train = np.array(XS_train)
+XS_train = np.array(XS_train) # shape (num_samples, num_channels, points per channel)
 
 keff_train_mean = np.mean(keff_train)
 keff_train_std = np.std(keff_train)
-
 y_train = zscore(keff_train)
-
-scaling_matrix_xtrain = XS_train.transpose()
-
-scaled_columns_xtrain = []
-print('Scaling training data...')
-
-le_bound_index = 1 # filters out NaNs
-
-training_column_means = []
-training_column_stds = []
-
-for column in tqdm.tqdm(scaling_matrix_xtrain[le_bound_index:-1], total=len(scaling_matrix_xtrain[le_bound_index:-1])):
-
-    column_mean = np.mean(column)
-    column_std = np.std(column)
-    training_column_means.append(column_mean)
-    training_column_stds.append(column_std)
-
-    scaled_column = zscore(column)
-    scaled_columns_xtrain.append(scaled_column)
-
-scaled_columns_xtrain = np.array(scaled_columns_xtrain)
-X_train = scaled_columns_xtrain.transpose()
 
 XS_val = []
 keff_val = []
 
-print('\nFetching validation data...')
+print('Fetching test data...')
+
 
 with ProcessPoolExecutor(max_workers=data_processes) as executor:
-    futures = [executor.submit(fetch_data, test_file) for test_file in test_files]
+	futures = [executor.submit(fetch_data, test_file) for test_file in test_files]
 
-    for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
-        xs_values_test, keff_value_test = future.result()
-        XS_val.append(xs_values_test)
-        keff_val.append(keff_value_test)
+	for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+		xs_values_val, keff_value_val = future.result()
+		XS_val.append(xs_values_val)
+		keff_val.append(keff_value_val)
 
 XS_val = np.array(XS_val)
 y_val = (np.array(keff_val) - keff_train_mean) / keff_train_std
 
-scaling_matrix_xtest = XS_val.transpose()
 
-scaled_columns_xtest = []
-print('\nScaling test data...')
-for column, c_mean, c_std in tqdm.tqdm(zip(scaling_matrix_xtest[le_bound_index:-1], training_column_means, training_column_stds), total=len(scaling_matrix_xtest[le_bound_index:-1])):
-	scaled_column_val = (np.array(column) - c_mean) / c_std
+print('Scaling training data...')
 
-	scaled_columns_xtest.append(scaled_column_val)
 
-scaled_columns_xtest = np.array(scaled_columns_xtest)
-X_val = scaled_columns_xtest.transpose()
+# le_bound_index = 1 # filters out NaNs
 
-test_mask = ~np.isnan(X_val).any(axis=0)
-X_val = X_val[:, test_mask]
+def process_data(XS_train, XS_val):
 
-train_mask = ~np.isnan(X_train).any(axis=0)
-X_train = X_train[:, train_mask]
+
+	channel_matrix_train = [[] for i in range(len(XS_train[0]))] # each element is a matrix of only one channel, e.g. channel_matrix[0] is all the lists containing
+	channel_matrix_val = [[] for i in range(len(XS_val[0]))]
+	# Pu-239 (n,el)
+	scaled_channel_matrix_train = []
+	scaled_channel_matrix_val = []
+
+	for matrix in tqdm.tqdm(XS_train, total =len(XS_train)):
+		# Each matrix has shape (num channels, points per channel)
+		for channel_index, channel in enumerate(matrix):
+			channel_matrix_train[channel_index].append(channel)
+
+		# channel_matrix now has shape (num channels, num samples, points per channel)
+		# Each element of channel matrix has shape (num samples, points per channel)
+	for matrix in tqdm.tqdm(XS_val, total =len(XS_val)):
+		for channel_index, channel in enumerate(matrix):
+			channel_matrix_val[channel_index].append(channel)
+
+	#################################################################################################################################################################
+	for channel_data_train, channel_data_val in zip(channel_matrix_train, channel_matrix_val): # each iterative variable is the tensor of one specific channel e.g. Pu-239 fission, for all samples
+		transposed_matrix_train = np.transpose(channel_data_train) # shape (energy points per sample, num samples)
+		transposed_matrix_val = np.transpose(channel_data_val)
+
+		transposed_scaled_channel_train = []
+		transposed_scaled_channel_val = []
+		for energy_point_train, energy_point_val in zip(transposed_matrix_train[:-1], transposed_matrix_val[:-1]): # each point on the unionised energy grid
+
+			train_mean = np.mean(energy_point_train)
+			train_std = np.std(energy_point_train)
+
+			scaled_point_train = zscore(energy_point_train)
+			transposed_scaled_channel_train.append(scaled_point_train)
+
+			scaled_point_val = (np.array(energy_point_val) - train_mean) / train_std
+			transposed_scaled_channel_val.append(scaled_point_val)
+
+		scaled_channel_train = np.array(transposed_scaled_channel_train)
+		scaled_channel_train = scaled_channel_train.transpose()
+		scaled_channel_matrix_train.append(scaled_channel_train)
+
+		scaled_channel_val = np.array(transposed_scaled_channel_val)
+		scaled_channel_val = scaled_channel_val.transpose()
+		scaled_channel_matrix_val.append(scaled_channel_val)
+
+	###################################################################################################################################################################
+	# print('Forming scaled training data...')
+	X_matrix_train = [[] for i in range(XS_train.shape[0])] # number of samples
+	X_matrix_val = [[] for i in range(XS_val.shape[0])]
+
+	for scaled_observable in scaled_channel_matrix_train:
+		for sample_index, channel_sample in enumerate(scaled_observable):
+			X_matrix_train[sample_index].append(channel_sample)
+
+	for scaled_observable in scaled_channel_matrix_val:
+		for sample_index, channel_sample in enumerate(scaled_observable):
+			X_matrix_val[sample_index].append(channel_sample)
+
+	X_matrix_train = np.array(X_matrix_train)
+	X_matrix_train[np.isnan(X_matrix_train)] = 0
+
+	X_matrix_val = np.array(X_matrix_val)
+	X_matrix_val[np.isnan(X_matrix_val)] = 0 # changes nans to 0
+
+	return X_matrix_train, X_matrix_val
+
+X_train, X_val = process_data(XS_train, XS_val)
 
 T = X_val.shape[-1]
 
@@ -166,7 +214,6 @@ X_val = torch.from_numpy(X_val).float()
 y_val = torch.from_numpy(y_val).float()
 
 # Working with tensors now
-
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
