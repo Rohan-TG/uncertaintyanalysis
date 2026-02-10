ba8ab300ce05967cdfd22884f663afc227153665
Rohan-TG <rohan.tg05@gmail.com>
2026-02-10 15:26:37 +0000

    x test func

--- ml/random/cnn/random-1d-cnn.py
+++ ml/random/cnn/random-1d-cnn.py
@@ -21,6 +21,9 @@ import time
 
 
 data_directory = input('Data directory: ')
+test_data_directory = input('Test data directory (x for set to val): ')
+
+
 data_processes = int(input('Num. data processors: '))
 
 all_parquets = os.listdir(data_directory)
@@ -36,10 +39,12 @@ while len(training_files) < n_training_samples:
 	if choice not in training_files:
 		training_files.append(choice)
 
-test_files = []
+val_files = []
 for file in all_parquets:
 	if file not in training_files:
-		test_files.append(file)
+		val_files.append(file)
+
+
 
 print('Fetching training data...')
 
@@ -106,11 +111,11 @@ y_train = zscore(keff_train)
 XS_val = []
 keff_val = []
 
-print('Fetching test data...')
+print('Fetching val data...')
 
 
 with ProcessPoolExecutor(max_workers=data_processes) as executor:
-	futures = [executor.submit(fetch_data, test_file) for test_file in test_files]
+	futures = [executor.submit(fetch_data, val_file) for val_file in val_files]
 
 	for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
 		xs_values_val, keff_value_val = future.result()
@@ -121,19 +126,42 @@ XS_val = np.array(XS_val)
 y_val = (np.array(keff_val) - keff_train_mean) / keff_train_std
 
 
+if test_data_directory != 'x':
+	print('Fetching test data...')
+	test_files = os.listdir(test_data_directory)
+
+	XS_test = []
+	keff_test = []
+	with ProcessPoolExecutor(max_workers=data_processes) as executor:
+		futures_test = [executor.submit(fetch_data, test_file) for test_file in test_files]
+
+		for future_test in tqdm.tqdm(as_completed(futures_test), total=len(futures_test)):
+			xs_values_test, keff_value_test = future_test.result()
+			XS_test.append(xs_values_test)
+			keff_test.append(keff_value_test)
+
+	XS_test = np.array(XS_test)
+	y_test = (np.array(keff_test) - keff_train_mean) / keff_train_std
+
+
 print('Scaling training data...')
 
 
 # le_bound_index = 1 # filters out NaNs
 
-def process_data(XS_train, XS_val):
+def process_data(XS_train, XS_val, XS_test):
 
 
 	channel_matrix_train = [[] for i in range(len(XS_train[0]))] # each element is a matrix of only one channel, e.g. channel_matrix[0] is all the lists containing
 	channel_matrix_val = [[] for i in range(len(XS_val[0]))]
+	channel_matrix_test = [[] for i in range(len(XS_test[0]))]
+
+
 	# Pu-239 (n,el)
 	scaled_channel_matrix_train = []
 	scaled_channel_matrix_val = []
+	scaled_channel_matrix_test = []
+
 
 	for matrix in tqdm.tqdm(XS_train, total =len(XS_train)):
 		# Each matrix has shape (num channels, points per channel)
@@ -146,14 +174,20 @@ def process_data(XS_train, XS_val):
 		for channel_index, channel in enumerate(matrix):
 			channel_matrix_val[channel_index].append(channel)
 
+	for matrix in tqdm.tqdm(XS_test, total =len(XS_test)):
+		for channel_index, channel in enumerate(matrix):
+			channel_matrix_test[channel_index].append(channel)
+
 	#################################################################################################################################################################
-	for channel_data_train, channel_data_val in zip(channel_matrix_train, channel_matrix_val): # each iterative variable is the tensor of one specific channel e.g. Pu-239 fission, for all samples
+	for channel_data_train, channel_data_val, channel_data_test in zip(channel_matrix_train, channel_matrix_val, channel_matrix_test): # each iterative variable is the tensor of one specific channel e.g. Pu-239 fission, for all samples
 		transposed_matrix_train = np.transpose(channel_data_train) # shape (energy points per sample, num samples)
 		transposed_matrix_val = np.transpose(channel_data_val)
+		transposed_matrix_test = np.transpose(channel_data_test)
 
 		transposed_scaled_channel_train = []
 		transposed_scaled_channel_val = []
-		for energy_point_train, energy_point_val in zip(transposed_matrix_train[:-1], transposed_matrix_val[:-1]): # each point on the unionised energy grid
+		transposed_scaled_channel_test = []
+		for energy_point_train, energy_point_val, energy_point_test in zip(transposed_matrix_train[:-1], transposed_matrix_val[:-1], transposed_matrix_test[:-1]): # each point on the unionised energy grid
 
 			train_mean = np.mean(energy_point_train)
 			train_std = np.std(energy_point_train)
@@ -164,6 +198,9 @@ def process_data(XS_train, XS_val):
 			scaled_point_val = (np.array(energy_point_val) - train_mean) / train_std
 			transposed_scaled_channel_val.append(scaled_point_val)
 
+			scaled_point_test = (np.array(energy_point_test) - train_mean) / train_std
+			transposed_scaled_channel_test.append(scaled_point_test)
+
 		scaled_channel_train = np.array(transposed_scaled_channel_train)
 		scaled_channel_train = scaled_channel_train.transpose()
 		scaled_channel_matrix_train.append(scaled_channel_train)
@@ -172,10 +209,15 @@ def process_data(XS_train, XS_val):
 		scaled_channel_val = scaled_channel_val.transpose()
 		scaled_channel_matrix_val.append(scaled_channel_val)
 
+		scaled_channel_test = np.array(transposed_scaled_channel_test)
+		scaled_channel_test = scaled_channel_test.transpose()
+		scaled_channel_matrix_test.append(scaled_channel_test)
+
 	###################################################################################################################################################################
 	# print('Forming scaled training data...')
 	X_matrix_train = [[] for i in range(XS_train.shape[0])] # number of samples
 	X_matrix_val = [[] for i in range(XS_val.shape[0])]
+	X_matrix_test = [[] for i in range(XS_test.shape[0])]
 
 	for scaled_observable in scaled_channel_matrix_train:
 		for sample_index, channel_sample in enumerate(scaled_observable):
@@ -185,15 +227,27 @@ def process_data(XS_train, XS_val):
 		for sample_index, channel_sample in enumerate(scaled_observable):
 			X_matrix_val[sample_index].append(channel_sample)
 
+	for scaled_observable in scaled_channel_matrix_test:
+		for sample_index, channel_sample in enumerate(scaled_observable):
+			X_matrix_test[sample_index].append(channel_sample)
+
 	X_matrix_train = np.array(X_matrix_train)
 	X_matrix_train[np.isnan(X_matrix_train)] = 0
 
 	X_matrix_val = np.array(X_matrix_val)
 	X_matrix_val[np.isnan(X_matrix_val)] = 0 # changes nans to 0
 
-	return X_matrix_train, X_matrix_val
+	X_matrix_test = np.array(X_matrix_test)
+	X_matrix_test[np.isnan(X_matrix_test)] = 0
 
-X_train, X_val = process_data(XS_train, XS_val)
+	return X_matrix_train, X_matrix_val, X_matrix_test
+
+
+if test_data_directory == 'x':
+	XS_test = XS_val
+	y_test = y_val
+
+X_train, X_val, X_test = process_data(XS_train, XS_val, XS_test)
 
 
 
