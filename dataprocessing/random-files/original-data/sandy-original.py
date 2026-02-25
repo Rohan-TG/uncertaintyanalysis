02791a295649762341ff57344919594d327b6e28
Rohan-TG <rohan.tg05@gmail.com>
2026-02-25 15:52:55 +0000

    ga71

--- dataprocessing/random-files/original-data/sandy-original.py
+++ dataprocessing/random-files/original-data/sandy-original.py
@@ -8,9 +8,9 @@ if computer == 'fermiac':
 	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/')
 elif computer == 'oppie':
 	sys.path.append('/home/rnt26/uncertaintyanalysis/')
-from groupEnergies import Ga69
+from groupEnergies import Ga71
 
-nuclide = Ga69
+nuclide = Ga71
 lib_name = "ENDFB_80"
 nucl = nuclide.ZA * 10
 filename = f"{nuclide.ZA}_{lib_name}.pendf"
