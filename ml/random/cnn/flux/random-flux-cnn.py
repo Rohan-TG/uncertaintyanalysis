3f1810179d14e3e5660e2de2ab385f5b8f09c2e2
Rohan-TG <rohan.tg05@gmail.com>
2026-03-04 18:40:46 +0000

    analysis added

--- ml/random/cnn/flux/random-flux-cnn.py
+++ ml/random/cnn/flux/random-flux-cnn.py
@@ -372,8 +372,8 @@ predictions = model.predict(X_val)
 from sklearn.metrics import r2_score
 r2s = []
 
-rescaled_full_p = []
-rescaled_y_val = []
+rescaled_full_p = [] # contains predictions
+rescaled_y_val = [] # contains labels
 pct_list = []
 for idx, (p_set, true_set) in enumerate(zip(predictions, y_val)):
 	rescaled_predictions = descaler(p_set, means=scaling_means, stds=scaling_stds)
@@ -382,34 +382,70 @@ for idx, (p_set, true_set) in enumerate(zip(predictions, y_val)):
 	rescaled_true_set = descaler(true_set, means=scaling_means, stds=scaling_stds)
 	rescaled_y_val.append(rescaled_true_set)
 
+	true_percentage_errors = 100 * (np.array(flux_errors_val[idx]) / np.array(rescaled_true_set))
+
 	ratios = np.array(rescaled_predictions) / np.array(rescaled_true_set)
 	pct_deviation = (ratios - 1.0) * 100
 	pct_list.append(pct_deviation)
-	print(f'{idx} - Mean: {np.mean(ratios):0.4f} Max: {max(ratios):0.4f} Min: {min(ratios):0.4f} R2: {r2_score(rescaled_predictions, rescaled_true_set):0.5f}')
+
+	count = 0
+	for point_ml_error, point_real_error in zip(pct_deviation, true_percentage_errors):
+		if point_ml_error > point_real_error:
+			count += 1
+	over_limit_pct = (count / len(pct_deviation)) * 100
+	print(f'{idx} - {over_limit_pct:0.1f}% Points over limit, Mean: {np.mean(ratios):0.4f} Max: {max(ratios):0.4f} Min: {min(ratios):0.4f} R2: {r2_score(rescaled_predictions, rescaled_true_set):0.5f}')
 	r2s.append(r2_score(rescaled_predictions, rescaled_true_set))
 
 print(f'Mean R2: {np.mean(r2s):0.5f}')
 
+d1, d2, d3 = fetch_data(all_parquets[0])
 
+grid = []
+for i in flux_lower_bounds:
+	grid.append(float(i))
+
+for j in flux_upper_bounds:
+	grid.append(float(j))
+
+grid.sort()
+edges = []
+for i in grid:
+	if i not in edges:
+		edges.append(i)
 
 def plot_index(idx):
 
-	lower_error_bound = np.array(rescaled_full_p[idx]) - np.array(flux_errors_val[idx])
-	upper_error_bound = np.array(rescaled_full_p[idx]) + np.array(flux_errors_val[idx])
+	# lower_error_bound = np.array(rescaled_full_p[idx]) - np.array(flux_errors_val[idx])
+	# upper_error_bound = np.array(rescaled_full_p[idx]) + np.array(flux_errors_val[idx])
 	x_axis = np.arange(0,300,1)
 
-	true_pct_error = 100 * (np.array(flux_errors_val[idx]) / np.array(rescaled_full_p[idx]))
+	true_pct_error = 100 * (np.array(flux_errors_val[idx]) / np.array(rescaled_y_val[idx]))
 
 	sigma_2_upper = 2* true_pct_error
 	sigma_2_lower = -1 * true_pct_error
 
+	# plt.figure()
+	# plt.plot(rescaled_full_p[idx], label='Prediction')
+	# plt.plot(rescaled_y_val[idx], label='True')
+	# plt.fill_between(x_axis, lower_error_bound, upper_error_bound, color='r', alpha=0.3)
+	# plt.legend()
+	# plt.grid()
+	# plt.savefig(f'{idx}.png')
+
+
+	widths = np.diff(edges)
 	plt.figure()
-	plt.plot(rescaled_full_p[idx], label='Prediction')
-	plt.plot(rescaled_y_val[idx], label='True')
-	plt.fill_between(x_axis, lower_error_bound, upper_error_bound, color='r', alpha=0.3)
-	plt.legend()
+	plt.bar(edges[:-1], rescaled_full_p[idx], width=widths, label='Prediction')
+	plt.bar(edges[:-1], rescaled_y_val[idx], width=widths, label = "True")
 	plt.grid()
-	plt.savefig(f'{idx}.png')
+	plt.legend()
+	plt.xlabel('Energy / MeV')
+	plt.ylabel('Normalised flux')
+	plt.xscale('log')
+	plt.ylim(0,0.015)
+	plt.savefig(f'{idx}_bar.png')
+
+
 
 	plt.figure()
 	plt.plot(pct_list[idx], label = 'ML error')
