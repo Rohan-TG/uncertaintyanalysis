3cd472d43e834dbfbba545c922fda68c6a4ac73a
Rohan-TG <rohan.tg05@gmail.com>
2026-03-04 21:19:21 +0000

    output forgotten

--- dataprocessing/jendl5/Gallium/gallium_ace_jendl5.py
+++ dataprocessing/jendl5/Gallium/gallium_ace_jendl5.py
@@ -13,3 +13,5 @@ elif computer == 'oppie':
 endf6 = sandy.Endf6.from_file('Ga_69_JENDL5.endf')
 pendf = endf6.get_pendf(err=0.0001, verbose=True)
 outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf)
+with open(f"Ga_69_JENDL5.03c", mode="w") as f:
+	f.write(outs["ace"])
