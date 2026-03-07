d5dc9fdc3f40e4e6b68a230832e27b6f15122337
Rohan-TG <rohan.tg05@gmail.com>
2026-03-07 13:22:49 +0000

    pendf

--- dataprocessing/jendl5/Gallium/gallium_ace_jendl5.py
+++ dataprocessing/jendl5/Gallium/gallium_ace_jendl5.py
@@ -13,7 +13,7 @@ elif computer == 'oppie':
 endf6 = sandy.Endf6.from_file('../Pu241/Pu_241_JENDL5.endf')
 pendf = endf6.get_pendf(err=0.0001, verbose=True)
 outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf)
-with open(f"Pu_240_JENDL5.03c", mode="w") as f:
+with open(f"Pu_241_JENDL5.03c", mode="w") as f:
 	f.write(outs["ace"])
 
 # pendf.to_file(f"Pu_241_JENDL5.pendf")
