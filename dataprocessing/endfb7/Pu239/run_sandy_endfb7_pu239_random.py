359e8be3ce9d265be8b60ed8e252c45ec9d7b6ef
Rohan-TG <rohan.tg05@gmail.com>
2026-03-07 13:33:40 +0000

    asdf

--- dataprocessing/endfb7/Pu239/run_sandy_endfb7_pu239_random.py
+++ dataprocessing/endfb7/Pu239/run_sandy_endfb7_pu239_random.py
@@ -17,45 +17,49 @@ filename = f"{nucl}.{lib_name}"
 
 endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
 # endf6.to_file(filename)
-# pendf = endf6.get_pendf(err=0.0001)
+pendf = endf6.get_pendf(err=0.0001)
 
 
-num_samples = 100  # number of samples
-processes = 5
+outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf)
+with open(f"Pu_239_ENDFBVII1.03c", mode="w") as f:
+	f.write(outs["ace"])
 
-
-
-samples = endf6.get_perturbations(
-	num_samples,
-	njoy_kws=dict(
-		err=0.0001,
-		chi=False,
-		mubar=False,
-		xs=True,
-		nubar=False,
-		verbose=True,
-	),
-)
-
-outs = endf6.apply_perturbations( # generates the PENDFs only
-	samples,
-	processes=processes,
-	njoy_kws=dict(err=0.0001),  # low error
-	# to_ace=True,   # produce ACE files
-	to_file=True,
-	# ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
-	verbose=True,
-)
-
-
-ace_outs = endf6.apply_perturbations(
-	samples,
-	processes=processes,
-	njoy_kws=dict(err=0.0001),
-	to_ace = True,
-	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
-	to_file=True,
-	verbose=True,
-)
-
-
+# num_samples = 100  # number of samples
+# processes = 5
+#
+#
+#
+# samples = endf6.get_perturbations(
+# 	num_samples,
+# 	njoy_kws=dict(
+# 		err=0.0001,
+# 		chi=False,
+# 		mubar=False,
+# 		xs=True,
+# 		nubar=False,
+# 		verbose=True,
+# 	),
+# )
+#
+# outs = endf6.apply_perturbations( # generates the PENDFs only
+# 	samples,
+# 	processes=processes,
+# 	njoy_kws=dict(err=0.0001),  # low error
+# 	# to_ace=True,   # produce ACE files
+# 	to_file=True,
+# 	# ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+# 	verbose=True,
+# )
+#
+#
+# ace_outs = endf6.apply_perturbations(
+# 	samples,
+# 	processes=processes,
+# 	njoy_kws=dict(err=0.0001),
+# 	to_ace = True,
+# 	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+# 	to_file=True,
+# 	verbose=True,
+# )
+#
+#
