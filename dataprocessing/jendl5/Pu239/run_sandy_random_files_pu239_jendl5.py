09f254d23d4f6855f3fec63e0c590f6189f2c5b8
Rohan-TG <rohan.tg05@gmail.com>
2026-03-12 13:53:10 +0000

    as

--- dataprocessing/jendl5/Pu239/run_sandy_random_files_pu239_jendl5.py
+++ dataprocessing/jendl5/Pu239/run_sandy_random_files_pu239_jendl5.py
@@ -8,17 +8,17 @@ if computer == 'fermiac':
 	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/')
 elif computer == 'oppie':
 	sys.path.append('/home/rnt26/uncertaintyanalysis/')
-from groupEnergies import Pu239
+from groupEnergies import Pu241
 
 
 lib_name = "JEFF_33"
-nucl = Pu239.ZA * 10
+nucl = Pu241.ZA * 10
 filename = f"{nucl}.{lib_name}"
 
-endf6 = sandy.Endf6.from_file('../Pu239/Pu_239_JENDL5.endf')
+endf6 = sandy.Endf6.from_file('../Pu241/Pu_241_JENDL5.endf')
 pendf = endf6.get_pendf(err=0.0001)
-pendf.to_file('94239_-1.pendf')
+pendf.to_file('94241_-1.pendf')
 
 outs = endf6.get_ace(temperature=300, heatr=False, thermr=False, gaspr=False, purr=True, verbose=True, pendf=pendf)
-with open(f"94239_-1.03c", mode="w") as f:
+with open(f"94241_-1.03c", mode="w") as f:
 	f.write(outs["ace"])
