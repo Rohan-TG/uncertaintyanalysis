d61ba25038af72acedc43282d79130f719194e04
Rohan-TG <rohan.tg05@gmail.com>
2026-03-14 21:11:27 +0000

    imports

--- feature-engineering/eta/calculate_eta.py
+++ feature-engineering/eta/calculate_eta.py
@@ -1,1 +1,11 @@
 import os
+import sys
+computer = os.uname().nodename
+if computer == 'fermiac':
+	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/') # change depending on machine
+elif computer == 'oppie':
+	sys.path.append('/home/rnt26/uncertaintyanalysis/')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
