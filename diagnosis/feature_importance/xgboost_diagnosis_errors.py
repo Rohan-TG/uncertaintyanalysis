0da5935addd608240df2984adc0daa7f396ff3d2
Rohan-TG <rohan.tg05@gmail.com>
2026-03-17 00:45:46 +0000

    run on fermiac or oppie

--- diagnosis/feature_importance/xgboost_diagnosis_errors.py
+++ diagnosis/feature_importance/xgboost_diagnosis_errors.py
@@ -1,6 +1,6 @@
 import os
-
-import xgboost
+import tqdm
+import xgboost as xg
 import pandas as pd
 import random
 
@@ -11,6 +11,8 @@ all_files = os.listdir(error_data_directory)
 
 training_fraction = float(input('Training fraction: '))
 n_training_samples = int(training_fraction * len(all_files))
+
+lower_energy_bound = float(input('Lower energy bound eV: '))
 
 training_files = []
 while len(training_files) < n_training_samples:
@@ -24,3 +26,97 @@ for f in all_files:
 	if f not in training_files:
 		validation_files.append(f)
 
+
+def fetch_data(datafile, data_dir=error_data_directory):
+
+	temp_df = pd.read_parquet(f'{data_dir}/{datafile}', engine='pyarrow')
+	temp_df = temp_df[temp_df['ERG'] >= lower_energy_bound]
+
+	keff_value = float(temp_df['keff'].values[0])
+
+	pu9_mt18xs = temp_df['94239_MT18_XS'].values.tolist()
+	pu0_mt18xs = temp_df['94240_MT18_XS'].values.tolist()
+	pu1_mt18xs = temp_df['94241_MT18_XS'].values.tolist()
+
+	pu9_mt2xs = temp_df['94239_MT2_XS'].values.tolist()
+	pu0_mt2xs = temp_df['94240_MT2_XS'].values.tolist()
+	pu1_mt2xs = temp_df['94241_MT2_XS'].values.tolist()
+
+	pu9_mt4xs = temp_df['94239_MT4_XS'].values.tolist()
+	pu0_mt4xs = temp_df['94240_MT4_XS'].values.tolist()
+	pu1_mt4xs = temp_df['94241_MT4_XS'].values.tolist()
+
+	pu9_mt16xs = temp_df['94239_MT16_XS'].values.tolist()
+	pu0_mt16xs = temp_df['94240_MT16_XS'].values.tolist()
+	pu1_mt16xs = temp_df['94241_MT16_XS'].values.tolist()
+
+	pu9_mt102xs = temp_df['94239_MT102_XS'].values.tolist()
+	pu0_mt102xs = temp_df['94240_MT102_XS'].values.tolist()
+	pu1_mt102xs = temp_df['94241_MT102_XS'].values.tolist()
+
+	XS_obj = [pu9_mt2xs, pu9_mt4xs, pu9_mt16xs, pu9_mt18xs, pu9_mt102xs,
+				pu0_mt2xs, pu0_mt4xs, pu0_mt16xs, pu0_mt18xs, pu0_mt102xs,
+				pu1_mt2xs, pu1_mt4xs, pu1_mt16xs, pu1_mt18xs, pu1_mt102xs,]
+
+	return(XS_obj, keff_value)
+
+
+channel_keys = {'94239_MT2_XS': 0,'94239_MT4_XS': 1,'94239_MT16_XS': 2,
+				'94239_MT18_XS': 3,'94239_MT102_XS': 4,'94240_MT2_XS': 5,
+				'94240_MT4_XS': 6,'94240_MT16_XS': 7,'94240_MT18_XS': 8,
+				'94240_MT102_XS': 9,'94241_MT2_XS': 10,'94241_MT4_XS': 11,
+				'94241_MT16_XS': 12,'94241_MT18_XS': 13,'94241_MT102_XS': 14,}
+
+
+print('\nFetching training data...\n')
+
+
+X_train = []
+y_train = []
+for train_f in tqdm.tqdm(training_files, total=len(training_files)):
+	xs_obj_train, labels_train = fetch_data(train_f)
+
+	X_train.append(xs_obj_train)
+	y_train.append(labels_train)
+
+
+print('\nFetching validation data...\n')
+
+X_val = []
+y_val = []
+for val_f in tqdm.tqdm(validation_files, total=len(validation_files)):
+	xs_obj_val, labels_val = fetch_data(val_f)
+
+	X_val.append(xs_obj_val)
+	y_val.append(labels_val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+model = xg.XGBRegressor(n_estimators=950, # define regressor
+						learning_rate=0.01,
+						max_depth=8,
+						subsample=0.18236,
+						max_leaves=0,
+						seed=42, )
+
+
+model.fit(X_train, y_train, verbose=True,
+		  early_stopping_rounds=10,
+		  eval_set = [(X_val, y_val)])
+
+print("\nTraining complete")
+
+predictions = model.predict(X_val)
+
+for error, predicted_error in zip(y_val, predictions):
+	print(f'Error: {error} --- Predicted error: {predicted_error} --- Difference: {error - predicted_error}')
