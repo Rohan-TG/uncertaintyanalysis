c31aa66e6447fff4dab9d1573d36b2780599f163
Rohan-TG <rohan.tg05@gmail.com>
2026-03-25 15:36:03 +0000

    trying way more nodes

--- ml/random/mlp/pytorch_mlp.py
+++ ml/random/mlp/pytorch_mlp.py
@@ -172,24 +172,24 @@ class MLP(nn.Module):
 	def __init__(self, input_dim):
 		super().__init__()
 		self.net = nn.Sequential(
-			nn.Linear(input_dim, 1000),  # Keras first Dense(1000), no activation
-			nn.Linear(1000, 900),
+			nn.Linear(input_dim, 2000),  # Keras first Dense(1000), no activation
+			nn.Linear(2000, 1800),
 			nn.ReLU(),
-			nn.Linear(900, 750),
+			nn.Linear(1800, 1500),
 			nn.ReLU(),
-			nn.Linear(750, 600),
+			nn.Linear(1500, 1200),
 			nn.ReLU(),
-			nn.Linear(600, 540),
+			nn.Linear(1200, 1000),
 			nn.ReLU(),
-			nn.Linear(540, 380),
+			nn.Linear(1000, 800),
 			nn.ReLU(),
-			nn.Linear(380, 280),
+			nn.Linear(800, 500),
 			nn.ReLU(),
-			nn.Linear(280, 150),
+			nn.Linear(500, 400),
 			nn.ReLU(),
-			nn.Linear(150, 100),
+			nn.Linear(400, 300),
 			nn.ReLU(),
-			nn.Linear(100, 1)  # linear output
+			nn.Linear(300, 1)  # linear output
 		)
 
 		self._init_weights()
@@ -234,7 +234,8 @@ y_train = torch.from_numpy(y_train).float()
 X_val = torch.from_numpy(X_val).float()
 y_val = torch.from_numpy(y_val).float()
 
-# If your data is numpy arrays:
+
+# Convert to tensors
 X_train_t = torch.tensor(X_train, dtype=torch.float32, device=device)
 y_train_t = torch.tensor(y_train, dtype=torch.float32, device=device).view(-1, 1)
 
