f2f0d91a0c3b17028bdf60ad3c94ccf608a24c40
Rohan-TG <rohan.tg05@gmail.com>
2026-03-26 13:48:09 +0000

    wip

---
+++ ml/random/mlp/oracle_mlp_tf.py
@@ -0,0 +1,31 @@
+import gc
+import os
+import matplotlib.pyplot as plt
+import keras.backend
+import pickle
+# MLP
+import sys
+computer = os.uname().nodename
+if computer == 'fermiac':
+	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/') # change depending on machine
+elif computer == 'oppie' or computer == 'bethe':
+	sys.path.append('/home/rnt26/uncertaintyanalysis/')
+import pandas as pd
+import random
+import numpy as np
+from scipy.stats import zscore, norm
+import tqdm
+import keras
+import time
+import tensorflow as tf
+import datetime
+
+print(tf.config.list_physical_devices('GPU'))
+
+
+data_directory = input('\n\nData directory: ')
+
+all_parquets = os.listdir(data_directory)
+
+
+model_directory = input('\nModel directory: ')
