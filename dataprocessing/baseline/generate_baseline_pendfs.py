24784e80516bc0dded8161a87447bad8531f0043
Rohan-TG <rohan.tg05@gmail.com>
2026-03-28 15:33:29 +0000

    ga71

--- dataprocessing/baseline/generate_baseline_pendfs.py
+++ dataprocessing/baseline/generate_baseline_pendfs.py
@@ -12,16 +12,16 @@ from groupEnergies import Pu239, Pu240, Pu241, Ga69, Ga71
 
 
 lib_name = "ENDFB_81"
-nucl = Ga69.ZA * 10
+nucl = Ga71.ZA * 10
 filename = f"{nucl}.{lib_name}"
 
 # endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
-endf_name = 'n-031_Ga_069.endf'
+endf_name = 'n-031_Ga_071.endf'
 endf6 = sandy.Endf6.from_file(f'/home/rnt26/uncertaintyanalysis/dataprocessing/baseline/endfbviii.1/neutrons-version.VIII.1/{endf_name}')
 # endf6.to_file(filename)
 pendf = endf6.get_pendf(err=0.0001)
 
-pendf.to_file('Ga-69_ENDFBVIII_1.pendf')
+pendf.to_file('Ga-71_ENDFBVIII_1.pendf')
 
 
 
@@ -33,6 +33,6 @@ outs = endf6.get_ace(temperature=300,
 					 verbose=True,
 					 pendf=pendf)
 
-savefilename = f"Ga-69_ENDFBVIII_1.03c"
+savefilename = f"Ga-71_ENDFBVIII_1.03c"
 with open(f"{savefilename}", mode="w") as f:
 	f.write(outs["ace"])
