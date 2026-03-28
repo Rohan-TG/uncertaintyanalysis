cd0c0c520b0e7949368dd24ed5cfcc9a22053e71
Rohan-TG <rohan.tg05@gmail.com>
2026-03-28 15:20:06 +0000

    adapted for endfbviii.1

--- dataprocessing/baseline/generate_baseline_pendfs.py
+++ dataprocessing/baseline/generate_baseline_pendfs.py
@@ -11,12 +11,28 @@ elif computer == 'oppie':
 from groupEnergies import Pu239, Pu240, Pu241
 
 
-lib_name = "ENDFB_80"
-nucl = Pu241.ZA * 10
+lib_name = "ENDFB_81"
+nucl = Pu239.ZA * 10
 filename = f"{nucl}.{lib_name}"
 
-endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
+# endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
+endf_name = 'n-094_Pu_239.endf'
+endf6 = sandy.Endf6.from_file(f'/home/rnt26/uncertaintyanalysis/dataprocessing/baseline/endfbviii.1/neutrons-version.VIII.1/{endf_name}')
 # endf6.to_file(filename)
 pendf = endf6.get_pendf(err=0.0001)
 
-pendf.to_file('Pu-241_ENDFBVIII.pendf')
+pendf.to_file('Pu-239_ENDFBVIII_1.pendf')
+
+
+
+outs = endf6.get_ace(temperature=300,
+					 heatr=False,
+					 thermr=False,
+					 gaspr=False,
+					 purr=True,
+					 verbose=True,
+					 pendf=pendf)
+
+savefilename = f"Pu239_ENDFBVIII_1.03c"
+with open(f"{savefilename}", mode="w") as f:
+	f.write(outs["ace"])
