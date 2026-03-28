f09e95160383bb6ded580d5bfcd5bcb98cdc61b0
Rohan-TG <rohan.tg05@gmail.com>
2026-03-28 15:31:32 +0000

    ga69

--- dataprocessing/baseline/generate_baseline_pendfs.py
+++ dataprocessing/baseline/generate_baseline_pendfs.py
@@ -8,20 +8,20 @@ if computer == 'fermiac':
 	sys.path.append('/home/rnt26/PycharmProjects/uncertaintyanalysis/')
 elif computer == 'oppie':
 	sys.path.append('/home/rnt26/uncertaintyanalysis/')
-from groupEnergies import Pu239, Pu240, Pu241
+from groupEnergies import Pu239, Pu240, Pu241, Ga69, Ga71
 
 
 lib_name = "ENDFB_81"
-nucl = Pu241.ZA * 10
+nucl = Ga69.ZA * 10
 filename = f"{nucl}.{lib_name}"
 
 # endf6 = sandy.get_endf6_file(lib_name, 'xs', nucl)
-endf_name = 'n-094_Pu_241.endf'
+endf_name = 'n-094_Ga69.endf'
 endf6 = sandy.Endf6.from_file(f'/home/rnt26/uncertaintyanalysis/dataprocessing/baseline/endfbviii.1/neutrons-version.VIII.1/{endf_name}')
 # endf6.to_file(filename)
 pendf = endf6.get_pendf(err=0.0001)
 
-pendf.to_file('Pu-241_ENDFBVIII_1.pendf')
+pendf.to_file('Ga-69_ENDFBVIII_1.pendf')
 
 
 
@@ -33,6 +33,6 @@ outs = endf6.get_ace(temperature=300,
 					 verbose=True,
 					 pendf=pendf)
 
-savefilename = f"Pu241_ENDFBVIII_1.03c"
+savefilename = f"Ga-69_ENDFBVIII_1.03c"
 with open(f"{savefilename}", mode="w") as f:
 	f.write(outs["ace"])
