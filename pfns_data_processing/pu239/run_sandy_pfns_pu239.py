2e09d673f227a620293b4683e43a292cab5736e2
Rohan-TG <rohan.tg05@gmail.com>
2026-04-07 07:27:57 +0100

    testing on oppie

--- pfns_data_processing/pu239/run_sandy_pfns_pu239.py
+++ pfns_data_processing/pu239/run_sandy_pfns_pu239.py
@@ -1,3 +1,29 @@
 import sandy
 import os
 import numpy as np
+
+za = 94239
+
+endf6 = sandy.get_endf6_file("ENDFB_80", "xs", za * 10)
+filename = '942390.ENDFB8_0'
+
+num_samples = 10  # number of samples
+processes = 5
+
+
+cli = f"{filename}  --processes {processes}  --samples {num_samples}  --mf 35  --temperatures 300  --acer  --debug"
+sandy.sampling.run(cli.split())
+
+#
+# samples = endf6.get_perturbations(
+# 	num_samples,
+# 	njoy_kws=dict(
+# 		err=0.0001,
+# 		chi=False,
+# 		mubar=False,
+# 		xs=False,
+# 		nubar=False,
+# 		verbose=True,
+# 	),
+# )
+
