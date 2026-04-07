268ec74f7949461ac4bd35ccf343a57533844099
Rohan-TG <rohan.tg05@gmail.com>
2026-04-07 21:23:37 +0100

    production of both PFNS perturbations and MF33 seems to be working

--- pfns_data_processing/pu239/run_sandy_pfns_pu239.py
+++ pfns_data_processing/pu239/run_sandy_pfns_pu239.py
@@ -29,3 +29,25 @@ samples = endf6.get_perturbations(
 	),
 )
 
+
+outs = endf6.apply_perturbations( # generates the PENDFs only
+	samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),  # low error
+	# to_ace=True,   # produce ACE files
+	to_file=True,
+	# ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+	verbose=True,
+)
+
+
+ace_outs = endf6.apply_perturbations(
+	samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),
+	to_ace = True,
+	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+	to_file=True,
+	verbose=True,
+)
+
