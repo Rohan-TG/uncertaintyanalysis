1db40f1c581252d3cbffc2a3343c588ce463b2f6
Rohan-TG <rohan.tg05@gmail.com>
2026-04-19 00:44:21 +0200

    testing

--- pfns_data_processing/pu239/xs_and_pfns/run_sandy_pfns_xs_samples.py
+++ pfns_data_processing/pu239/xs_and_pfns/run_sandy_pfns_xs_samples.py
@@ -2,6 +2,8 @@ import sandy
 import os
 import numpy as np
 import subprocess
+import endf
+import tqdm
 
 za = 94239
 
@@ -42,7 +44,45 @@ filelist = os.listdir(os.getcwd())
 if 'endf6_directory' not in filelist:
 	subprocess.run('mkdir endf6_directory', shell=True)
 
+if 'pendf_directory' not in filelist:
+	subprocess.run('mkdir pendf_directory', shell=True)
+
 subprocess.run('mv *.endf6 endf6_directory', shell=True)
+subprocess.run('rm *.pendf', shell=True)
+
+perturbed_mf5_endf6_files = os.listdir(f'{os.getcwd()}/endf6_directory')
+
+
+
+num_xs_samples = 1
+# perturb MF=3
+for perturbed_endf6 in tqdm.tqdm(perturbed_mf5_endf6_files, total=len(perturbed_mf5_endf6_files)):
+
+	perturbed_endf6_object = sandy.Endf6.from_file(f'endf6_directory/{perturbed_endf6}')
+
+	mf3_samples = perturbed_endf6_object.get_perturbations(
+		num_xs_samples,
+		njoy_kws=dict(
+			err=0.0001,
+			chi=False,
+			mubar=False,
+			xs=True,
+			nubar=False,
+			verbose=True,
+			to_file=True,
+		)
+	)
+
+	mf3_outs = perturbed_endf6_object.apply_perturbations(
+		mf3_samples,
+		processes=1,
+		njoy_kws=dict(err=0.0001),
+		to_file = True,
+		verbose = True,
+	)
+
+subprocess.run('mv *.pendf pendf_directory', shell=True)
+
 # ace_outs = endf6.apply_perturbations(
 # 	samples,
 # 	processes=processes,
