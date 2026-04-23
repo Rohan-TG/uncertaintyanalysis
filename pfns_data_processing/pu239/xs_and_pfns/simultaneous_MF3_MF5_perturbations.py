26a3e6851b4366ee6dd9320e30127b881b973bd2
Rohan-TG <rohan.tg05@gmail.com>
2026-04-23 16:06:20 +0200

    testing on oppie

--- pfns_data_processing/pu239/xs_and_pfns/simultaneous_MF3_MF5_perturbations.py
+++ pfns_data_processing/pu239/xs_and_pfns/simultaneous_MF3_MF5_perturbations.py
@@ -14,3 +14,35 @@ num_samples = 5  # number of samples
 processes = 5
 
 
+
+mf3_and_mf5_samples = endf6.get_perturbations(
+	num_samples,
+	njoy_kws=dict(
+		err=0.0001,
+		chi=True,
+		mubar=False,
+		xs=True,
+		nubar=False,
+		verbose=True,
+		to_file=True,
+	),
+)
+
+
+mf3_and_mf5_pendf_outs = endf6.apply_perturbations( # generates the PENDFs only
+	mf3_and_mf5_samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),  # low error
+	to_file=True,
+	verbose=True,
+)
+
+mf3_and_mf5_ace_outs = endf6.apply_perturbations(
+	mf3_and_mf5_samples,
+	processes=processes,
+	njoy_kws=dict(err=0.0001),
+	to_ace=True,
+	ace_kws=dict(err=0.0001, temperature=300, verbose=True, purr=True, heatr=False, thermr=False, gaspr=False),
+	verbose=True,
+	to_file=True,
+	)
