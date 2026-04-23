e8cfb57df32c4f31a2c9a211dc5ece8045ae592b
Rohan-TG <rohan.tg05@gmail.com>
2026-04-23 16:21:21 +0200

    asdf

--- pfns_data_processing/pu239/scrape_pfns_data.py
+++ pfns_data_processing/pu239/scrape_pfns_data.py
@@ -13,10 +13,8 @@ def read_mf5(endf6_file, MT=18):
 		energies = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].x
 		y_values = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].y
 
+incident_energies = mat.section_data[5, 18]['subsections'][0]['distribution']['E']
 
-
-
-lambdas = list(range(1,24))
 y_matrix = []
 for i, set in enumerate(mat.section_data[5, 18]['subsections'][0]['distribution']['g']):
 	y_values = mat.section_data[5, 18]['subsections'][0]['distribution']['g'][i].y
@@ -25,12 +23,15 @@ for i, set in enumerate(mat.section_data[5, 18]['subsections'][0]['distribution'
 	x = mat.section_data[5, 18]['subsections'][0]['distribution']['g'][i].x
 
 
-
-facecolors = plt.colormaps['viridis_r'](np.linspace(0, 1, len(lambdas)))
+# Plotting
+facecolors = plt.colormaps['viridis_r'](np.linspace(0, 1, len(incident_energies)))
 ax = plt.figure().add_subplot(projection='3d')
-
-for i, l in enumerate(lambdas):
+for i, l in enumerate(incident_energies):
 	ax.fill_between(x, l, y_matrix[i], x, l, 0, facecolors=facecolors[i], alpha=.7)
-
-
+ax.set_xlabel('Outbound E / eV')
+ax.set_ylabel('Incident E / eV')
+ax.set_zlabel('Probability')
+plt.title('Pu-239 ENDF/B-VIII.0 PFNS')
+plt.savefig('PFNS_example_Pu-239.png', dpi=300)
+plt.show()
 
