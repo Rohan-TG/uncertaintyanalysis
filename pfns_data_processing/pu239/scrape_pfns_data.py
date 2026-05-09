25bde263d82ce0c0f7f9d79efa9ad4e4b2c8d4f3
Rohan-TG <rohan.tg05@gmail.com>
2026-05-09 20:49:35 +0100

    made into function

--- pfns_data_processing/pu239/scrape_pfns_data.py
+++ pfns_data_processing/pu239/scrape_pfns_data.py
@@ -3,35 +3,66 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = ('/Users/rntg/PycharmProjects/uncertaintyanalysis/feature-engineering/94239.endf6')
+# file = ('/Users/rntg/PycharmProjects/uncertaintyanalysis/feature-engineering/94239.endf6')
+master_file = '94239_master_file.ENDFB8_0'
+perturbed_endf6 = '94239_4.endf6'
 
-mat = endf.Material(file)
+# mat = endf.Material(file)
+matp = endf.Material(perturbed_endf6)
+mat_original = endf.Material(master_file)
+
+def read_mf5(mat, MT=18):
+	if MT != 455:
+		for i, set in enumerate(mat.section_data[5, MT]['subsections'][0]['distribution']['g']):
+			energies = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].x
+			y_values = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].y
+
+		incident_energies = mat.section_data[5, MT]['subsections'][0]['distribution']['E']
+
+		y_matrix = []
+		for i, set in enumerate(mat.section_data[5, MT]['subsections'][0]['distribution']['g']):
+			y_values = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].y
+			y_matrix.append(y_values)
+
+			x = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].x
+
+	else:
+		energies = mat.section_data[5, MT]['subsections'][0]['distribution']['g'].x
+		y_values = mat.section_data[5, MT]['subsections'][0]['distribution']['g'].y
+
+		incident_energies = [1]
+
+		y_matrix = []
+		y_values = mat.section_data[5, MT]['subsections'][0]['distribution']['g'].y
+		y_matrix.append(y_values)
+
+		x = mat.section_data[5, MT]['subsections'][0]['distribution']['g'].x
 
 
-def read_mf5(endf6_file, MT=18):
-	for i, set in enumerate(mat.section_data[5, 18]['subsections'][0]['distribution']['g']):
-		energies = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].x
-		y_values = mat.section_data[5, MT]['subsections'][0]['distribution']['g'][i].y
+	return incident_energies, y_matrix, x
 
-incident_energies = mat.section_data[5, 18]['subsections'][0]['distribution']['E']
+incident_energiesp, y_matrixp, xp = read_mf5(matp)
 
-y_matrix = []
-for i, set in enumerate(mat.section_data[5, 18]['subsections'][0]['distribution']['g']):
-	y_values = mat.section_data[5, 18]['subsections'][0]['distribution']['g'][i].y
-	y_matrix.append(y_values)
-
-	x = mat.section_data[5, 18]['subsections'][0]['distribution']['g'][i].x
+incident_energies_original, y_matrix_original, x_original = read_mf5(mat_original)
 
 
 # Plotting
-facecolors = plt.colormaps['viridis_r'](np.linspace(0, 1, len(incident_energies)))
+facecolors = plt.colormaps['viridis_r'](np.linspace(0, 1, len(incident_energiesp)))
 ax = plt.figure().add_subplot(projection='3d')
-for i, l in enumerate(incident_energies):
-	ax.fill_between(x, l, y_matrix[i], x, l, 0, facecolors=facecolors[i], alpha=.7)
+for i, l in enumerate(incident_energiesp):
+	ax.fill_between(xp, l, y_matrixp[i], xp, l, 0, facecolors=facecolors[i], alpha=.7)
 ax.set_xlabel('Outbound E / eV')
 ax.set_ylabel('Incident E / eV')
 ax.set_zlabel('Probability')
 plt.title('Pu-239 ENDF/B-VIII.0 PFNS')
-plt.savefig('PFNS_example_Pu-239.png', dpi=300)
+# plt.savefig('PFNS_example_Pu-239.png', dpi=300)
 plt.show()
 
+ratio_matrix = []
+for p, o in zip(y_matrixp, y_matrix_original):
+	ratio_matrix.append(np.array(p) / np.array(o))
+
+plt.figure()
+plt.plot(xp[:-1],ratio_matrix[0][:-1])
+plt.grid()
+plt.show()
