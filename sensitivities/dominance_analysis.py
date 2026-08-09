31bbc58c4d62afd5ed4dda1a45eba3de9c449325
Rohan-TG <rohan.tg05@gmail.com>
2026-08-09 22:40:00 +0100

    trying pca

--- sensitivities/dominance_analysis.py
+++ sensitivities/dominance_analysis.py
@@ -1,1 +1,26 @@
 import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+
+feature_names = [
+	"Pu9 Elastic",
+	"Pu9 Inelastic",
+	"Pu9 (n,2n)",
+	"Pu9 fission",
+	"Pu9 capture",
+
+	"Pu0 Elastic",
+	"Pu0 Inelastic",
+	"Pu0 (n,2n)",
+	"Pu0 fission",
+	"Pu0 capture",
+
+	"Pu1 Elastic",
+	"Pu1 Inelastic",
+	"Pu1 (n,2n)",
+	"Pu1 fission",
+	"Pu1 capture",
+]
+
+
+# analysis_df = df[feature_cols + ["keff"]]
